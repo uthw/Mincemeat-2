7d053597af7ca0a6bac539b1575ee4a170e8b46d
uthw <73855194+uthw@users.noreply.github.com>
2024-05-03 14:28:41 -0400

    Deepslate osmium ore needs iron pick, closes #7

--- kubejs/server_scripts/utilities/tagsBlock.js
+++ kubejs/server_scripts/utilities/tagsBlock.js
@@ -1,7 +1,13 @@
 ServerEvents.tags("block", (event) => {
-    const add = [["minecraft:needs_diamond_tool", "mekanism:osmium_ore"]];
+    const add = [
+        ["minecraft:needs_diamond_tool", "mekanism:osmium_ore"],
+        ["minecraft:needs_diamond_tool", "mekanism:deepslate_osmium_ore"],
+    ];
 
-    const remove = [["minecraft:needs_stone_tool", "mekanism:osmium_ore"]];
+    const remove = [
+        ["minecraft:needs_stone_tool", "mekanism:osmium_ore"],
+        ["minecraft:needs_stone_tool", "mekanism:deepslate_osmium_ore"],
+    ];
 
     add.forEach((addition) => {
         event.add(addition[0], addition[1]);
