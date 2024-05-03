c5890df4beba6eaca20388dc9c1acd34aa31110b
uthw <73855194+uthw@users.noreply.github.com>
2024-05-03 11:56:23 -0400

    apotheosis generation tweaks

--- kubejs/server_scripts/structures/apotheosis/boss_dungeon.js
+++ kubejs/server_scripts/structures/apotheosis/boss_dungeon.js
@@ -1,153 +1,153 @@
-// ServerEvents.highPriorityData((event) => {
-//     let dungeons = [
-//         {
-//             name: "boss_dungeon",
-//             "forge:conditions": [
-//                 {
-//                     type: "apotheosis:module",
-//                     module: "adventure",
-//                 },
-//             ],
-//             feature: "apotheosis:boss_dungeon",
-//             placement: [
-//                 {
-//                     type: "minecraft:count",
-//                     count: 1,
-//                 },
-//                 {
-//                     type: "minecraft:in_square",
-//                 },
-//                 {
-//                     type: "minecraft:height_range",
-//                     height: {
-//                         type: "minecraft:uniform",
-//                         max_inclusive: {
-//                             below_top: 0,
-//                         },
-//                         min_inclusive: {
-//                             absolute: 0,
-//                         },
-//                     },
-//                 },
-//                 {
-//                     type: "minecraft:biome",
-//                 },
-//             ],
-//         },
-//         {
-//             name: "boss_dungeon_2",
-//             "forge:conditions": [
-//                 {
-//                     type: "apotheosis:module",
-//                     module: "adventure",
-//                 },
-//             ],
-//             feature: "apotheosis:boss_dungeon_2",
-//             placement: [
-//                 {
-//                     type: "minecraft:count",
-//                     count: 1,
-//                 },
-//                 {
-//                     type: "minecraft:in_square",
-//                 },
-//                 {
-//                     type: "minecraft:height_range",
-//                     height: {
-//                         type: "minecraft:uniform",
-//                         max_inclusive: {
-//                             below_top: 0,
-//                         },
-//                         min_inclusive: {
-//                             absolute: 0,
-//                         },
-//                     },
-//                 },
-//                 {
-//                     type: "minecraft:biome",
-//                 },
-//             ],
-//         },
-//         {
-//             name: "boss_dungeon_deep",
-//             "forge:conditions": [
-//                 {
-//                     type: "apotheosis:module",
-//                     module: "adventure",
-//                 },
-//             ],
-//             feature: "apotheosis:boss_dungeon",
-//             placement: [
-//                 {
-//                     type: "minecraft:count",
-//                     count: 12,
-//                 },
-//                 {
-//                     type: "minecraft:in_square",
-//                 },
-//                 {
-//                     type: "minecraft:height_range",
-//                     height: {
-//                         type: "minecraft:uniform",
-//                         max_inclusive: {
-//                             absolute: -1,
-//                         },
-//                         min_inclusive: {
-//                             above_bottom: 6,
-//                         },
-//                     },
-//                 },
-//                 {
-//                     type: "minecraft:biome",
-//                 },
-//             ],
-//         },
-//         {
-//             name: "boss_dungeon_2_deep",
-//             "forge:conditions": [
-//                 {
-//                     type: "apotheosis:module",
-//                     module: "adventure",
-//                 },
-//             ],
-//             feature: "apotheosis:boss_dungeon_2",
-//             placement: [
-//                 {
-//                     type: "minecraft:count",
-//                     count: 12,
-//                 },
-//                 {
-//                     type: "minecraft:in_square",
-//                 },
-//                 {
-//                     type: "minecraft:height_range",
-//                     height: {
-//                         type: "minecraft:uniform",
-//                         max_inclusive: {
-//                             absolute: -1,
-//                         },
-//                         min_inclusive: {
-//                             above_bottom: 6,
-//                         },
-//                     },
-//                 },
-//                 {
-//                     type: "minecraft:biome",
-//                 },
-//             ],
-//         },
-//     ];
-//     // let dungeonTypes = [
-//     //     "boss_dungeon",
-//     //     "boss_dungeon_deep",
-//     //     "boss_dungeon_2",
-//     //     "boss_dungeon_2_deep",
-//     // ];
+ServerEvents.highPriorityData((event) => {
+    let dungeons = [
+        {
+            name: "boss_dungeon",
+            "forge:conditions": [
+                {
+                    type: "apotheosis:module",
+                    module: "adventure",
+                },
+            ],
+            feature: "apotheosis:boss_dungeon",
+            placement: [
+                {
+                    type: "minecraft:count",
+                    count: 1,
+                },
+                {
+                    type: "minecraft:in_square",
+                },
+                {
+                    type: "minecraft:height_range",
+                    height: {
+                        type: "minecraft:uniform",
+                        max_inclusive: {
+                            below_top: 0,
+                        },
+                        min_inclusive: {
+                            absolute: 0,
+                        },
+                    },
+                },
+                {
+                    type: "minecraft:biome",
+                },
+            ],
+        },
+        {
+            name: "boss_dungeon_2",
+            "forge:conditions": [
+                {
+                    type: "apotheosis:module",
+                    module: "adventure",
+                },
+            ],
+            feature: "apotheosis:boss_dungeon_2",
+            placement: [
+                {
+                    type: "minecraft:count",
+                    count: 1,
+                },
+                {
+                    type: "minecraft:in_square",
+                },
+                {
+                    type: "minecraft:height_range",
+                    height: {
+                        type: "minecraft:uniform",
+                        max_inclusive: {
+                            below_top: 0,
+                        },
+                        min_inclusive: {
+                            absolute: 0,
+                        },
+                    },
+                },
+                {
+                    type: "minecraft:biome",
+                },
+            ],
+        },
+        {
+            name: "boss_dungeon_deep",
+            "forge:conditions": [
+                {
+                    type: "apotheosis:module",
+                    module: "adventure",
+                },
+            ],
+            feature: "apotheosis:boss_dungeon",
+            placement: [
+                {
+                    type: "minecraft:count",
+                    count: 12,
+                },
+                {
+                    type: "minecraft:in_square",
+                },
+                {
+                    type: "minecraft:height_range",
+                    height: {
+                        type: "minecraft:uniform",
+                        max_inclusive: {
+                            absolute: -1,
+                        },
+                        min_inclusive: {
+                            above_bottom: 6,
+                        },
+                    },
+                },
+                {
+                    type: "minecraft:biome",
+                },
+            ],
+        },
+        {
+            name: "boss_dungeon_2_deep",
+            "forge:conditions": [
+                {
+                    type: "apotheosis:module",
+                    module: "adventure",
+                },
+            ],
+            feature: "apotheosis:boss_dungeon_2",
+            placement: [
+                {
+                    type: "minecraft:count",
+                    count: 12,
+                },
+                {
+                    type: "minecraft:in_square",
+                },
+                {
+                    type: "minecraft:height_range",
+                    height: {
+                        type: "minecraft:uniform",
+                        max_inclusive: {
+                            absolute: -1,
+                        },
+                        min_inclusive: {
+                            above_bottom: 6,
+                        },
+                    },
+                },
+                {
+                    type: "minecraft:biome",
+                },
+            ],
+        },
+    ];
+    let dungeonTypes = [
+        "boss_dungeon",
+        "boss_dungeon_deep",
+        "boss_dungeon_2",
+        "boss_dungeon_2_deep",
+    ];
 
-//     dungeons.forEach((dungeon) => {
-//         event.addJson(
-//             `apotheosis:worldgen/placed_feature/${dungeon.name}.json`,
-//             dungeon
-//         );
-//     });
-// });
+    dungeons.forEach((dungeon) => {
+        event.addJson(
+            `apotheosis:worldgen/placed_feature/${dungeon.name}.json`,
+            dungeon
+        );
+    });
+});
--- kubejs/server_scripts/structures/apotheosis/rogue_spawner.js
+++ kubejs/server_scripts/structures/apotheosis/rogue_spawner.js
@@ -1,40 +1,40 @@
-// ServerEvents.highPriorityData((event) => {
-//     let spawnerTweak = {
-//         "forge:conditions": [
-//             {
-//                 type: "apotheosis:module",
-//                 module: "adventure",
-//             },
-//         ],
-//         feature: "apotheosis:rogue_spawner",
-//         placement: [
-//             {
-//                 type: "minecraft:count",
-//                 count: 1, // changed value
-//             },
-//             {
-//                 type: "minecraft:in_square",
-//             },
-//             {
-//                 type: "minecraft:height_range",
-//                 height: {
-//                     type: "minecraft:uniform",
-//                     max_inclusive: {
-//                         below_top: 0,
-//                     },
-//                     min_inclusive: {
-//                         absolute: 0,
-//                     },
-//                 },
-//             },
-//             {
-//                 type: "minecraft:biome",
-//             },
-//         ],
-//     };
+ServerEvents.highPriorityData((event) => {
+    let spawnerTweak = {
+        "forge:conditions": [
+            {
+                type: "apotheosis:module",
+                module: "adventure",
+            },
+        ],
+        feature: "apotheosis:rogue_spawner",
+        placement: [
+            {
+                type: "minecraft:count",
+                count: 1, // changed value
+            },
+            {
+                type: "minecraft:in_square",
+            },
+            {
+                type: "minecraft:height_range",
+                height: {
+                    type: "minecraft:uniform",
+                    max_inclusive: {
+                        below_top: 0,
+                    },
+                    min_inclusive: {
+                        absolute: 0,
+                    },
+                },
+            },
+            {
+                type: "minecraft:biome",
+            },
+        ],
+    };
 
-//     event.addJson(
-//         `apotheosis:worldgen/placed_feature/rogue_spawner.json`,
-//         spawnerTweak
-//     );
-// });
+    event.addJson(
+        `apotheosis:worldgen/placed_feature/rogue_spawner.json`,
+        spawnerTweak
+    );
+});
