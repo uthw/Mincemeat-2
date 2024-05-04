00e2c7d65f0c0ec0ce20a84a3b541a1d35e8a2ac
uthw <73855194+uthw@users.noreply.github.com>
2024-05-04 12:42:59 -0400

    Towers are rarer

--- kubejs/server_scripts/structures/apotheosis/tower.js
+++ kubejs/server_scripts/structures/apotheosis/tower.js
@@ -1,38 +1,122 @@
 // the code of this is causing some error
 
-// ServerEvents.highPriorityData((event) => {
-//     let towerTweak = {
-//         "forge:conditions": [
-//             {
-//                 type: "apotheosis:module",
-//                 module: "adventure",
-//             },
-//         ],
-//         structures: [
-//             {
-//                 // structure: "apotheosis:tower_spruce",
-//                 weight: 1,
-//             },
-//         ],
-//         placement: {
-//             type: "minecraft:random_spread",
-//             spacing: 80,
-//             separation: 30,
-//         },
-//     };
+ServerEvents.highPriorityData((event) => {
+    let towerTweakMain = {
+        "forge:conditions": [
+            {
+                type: "apotheosis:module",
+                module: "adventure",
+            },
+        ],
+        structures: [
+            {
+                structure: "apotheosis:tower_main",
+                weight: 1,
+            },
+        ],
+        placement: {
+            type: "minecraft:random_spread",
+            spacing: 60,
+            separation: 30,
+            salt: 12121818,
+        },
+    };
 
-//     let towerTypes = ["_main", "_spruce", "_sand", "_leaf"];
+    let towerTweakLeaf = {
+        "forge:conditions": [
+            {
+                type: "apotheosis:module",
+                module: "adventure",
+            },
+        ],
+        structures: [
+            {
+                structure: "apotheosis:tower_leaf",
+                weight: 1,
+            },
+        ],
+        placement: {
+            type: "minecraft:random_spread",
+            spacing: 60,
+            separation: 30,
+            salt: 11114412,
+        },
+    };
 
-//     let towerSalts = [12121818, 89021723, 15192020, 11114412];
+    let towerTweakSand = {
+        "forge:conditions": [
+            {
+                type: "apotheosis:module",
+                module: "adventure",
+            },
+        ],
+        structures: [
+            {
+                structure: "apotheosis:tower_sand",
+                weight: 1,
+            },
+        ],
+        placement: {
+            type: "minecraft:random_spread",
+            spacing: 60,
+            separation: 30,
+            salt: 15192020,
+        },
+    };
 
-//     for (let i = 0; i < towerTypes.length; i++) {
-//         towerTweak.placement.salt = towerSalts[i];
-//         towerTweak.structures.structure = `apotheosis:tower${towerTypes[i]}`;
+    let towerTweakSpruce = {
+        "forge:conditions": [
+            {
+                type: "apotheosis:module",
+                module: "adventure",
+            },
+        ],
+        structures: [
+            {
+                structure: "apotheosis:tower_spruce",
+                weight: 1,
+            },
+        ],
+        placement: {
+            type: "minecraft:random_spread",
+            spacing: 60,
+            separation: 30,
+            salt: 89021723,
+        },
+    };
 
-//         // Hopefully this works :p
-//         event.addJson(
-//             `apotheosis:worldgen/structure_set/tower${towerTypes[i]}.json`,
-//             towerTweak
-//         );
-//     }
-// });
+    event.addJson(
+        "apotheosis:worldgen/structure_set/tower_main.json",
+        towerTweakMain
+    );
+
+    event.addJson(
+        "apotheosis:worldgen/structure_set/tower_leaf.json",
+        towerTweakLeaf
+    );
+
+    event.addJson(
+        "apotheosis:worldgen/structure_set/tower_sand.json",
+        towerTweakSand
+    );
+
+    event.addJson(
+        "apotheosis:worldgen/structure_set/tower_spruce.json",
+        towerTweakSpruce
+    );
+
+    // let towerTypes = ["_main", "_spruce", "_sand", "_leaf"];
+
+    // let towerSalts = [12121818, 89021723, 15192020, 11114412];
+
+    // for (let i = 0; i < towerTypes.length; i++) {
+    //     towerTweak.placement.salt = towerSalts[i];
+    //     towerTweak.structures.structure = `apotheosis:tower${towerTypes[i]}`;
+
+    //     // Hopefully this works :p
+    //     event.addJson(
+    //         `apotheosis:worldgen/structure_set/tower${towerTypes[i]}.json`,
+    //         towerTweak
+    //     );
+    // }
+});
