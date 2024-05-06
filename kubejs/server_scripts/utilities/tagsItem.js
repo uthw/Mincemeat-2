28ec5348fc4760b90698b31910d4f60f3f8eeaa4
uthw <73855194+uthw@users.noreply.github.com>
2024-05-05 20:06:05 -0400

    Add tag for death worm chitins

--- kubejs/server_scripts/utilities/tagsItem.js
+++ kubejs/server_scripts/utilities/tagsItem.js
@@ -2,6 +2,9 @@ ServerEvents.tags("item", (event) => {
     const add = [
         // Get [0] tag collection and add [1] to it
         // ["farmersdelight:tools/knives", "#forge:tools/knives"],
+        ["forge:death_worm_chitins", "iceandfire:deathworm_chitin_yellow"],
+        ["forge:death_worm_chitins", "iceandfire:deathworm_chitin_white"],
+        ["forge:death_worm_chitins", "iceandfire:deathworm_chitin_red"],
     ];
 
     add.forEach((addition) => {
