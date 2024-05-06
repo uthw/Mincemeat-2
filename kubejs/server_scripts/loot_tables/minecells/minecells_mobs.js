6bee5ab2efe0a7cfc46520669ee14c01e50be443
uthw <73855194+uthw@users.noreply.github.com>
2024-05-05 20:02:53 -0400

    wait that doesn't work

---
+++ kubejs/server_scripts/loot_tables/minecells/minecells_mobs.js
@@ -0,0 +1,22 @@
+// let mobs = [
+//     "minecells:buzzcutter",
+//     "minecells:disgusting_worm",
+//     "minecells:grenadier",
+//     "minecells:inquisitor",
+//     "minecells:kamikaze",
+//     "minecells:leaping_zombie",
+//     "minecells:mutated_bat",
+//     "minecells:rancid_rat",
+//     "minecells:runner",
+//     "minecells:scorpion",
+//     "minecells:shieldbearer",
+//     "minecells:shocker",
+//     "minecells:sweeper",
+//     "minecells:undead_archer",
+// ];
+
+// mobs.forEach((mob) => {
+//     EntityEvents.drops((mob) => {
+//         mob.addDrop("kubejs:cell", 0.25);
+//     });
+// });
