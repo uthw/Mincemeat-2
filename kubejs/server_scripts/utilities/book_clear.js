3e38da3d185d0107a3836e2d9cf4514945dbb8d7
uthw <73855194+uthw@users.noreply.github.com>
2024-05-05 20:05:58 -0400

    Fix items not being cleared if user has no OP

--- kubejs/server_scripts/utilities/book_clear.js
+++ kubejs/server_scripts/utilities/book_clear.js
@@ -1,13 +1,29 @@
-// PlayerEvents.advancement(event => {
-//     if (event.getAdvancement() != "iceandfire:iceandfire/root") {
-//         return;
-//     }
+PlayerEvents.advancement((event) => {
+    if (event.getAdvancement() != "iceandfire:iceandfire/root") {
+        return;
+    }
 
-//     // This advancement is granted right after a player loads into a world
-//     Utils.server.runCommandSilent("clear @p patchouli:guide_book")
+    // This advancement is granted right after a player loads into a world
+    // Utils.server.runCommandSilent("clear @p patchouli:guide_book");
 
-//     // initial inventory
+    // initial inventory
+    event.server.runCommandSilent("clear @p patchouli:guide_book");
+    event.server.runCommandSilent("give @p ftbquests:book");
+    event.server.runCommandSilent("clear @p enigmaticlegacy:cursed_ring");
+    event.server.runCommandSilent(
+        'give @p delightful:smore{RepairCost:0,display:{Name:\'{"text":"Starting Gift"}\'}}'
+    );
+    // Item.of(
+    //     "delightful:cheeseburger",
+    //     '{RepairCost:0,display:{Name:\'{"text":"Starting Gift 2"}\'}}'
+    // );
 
-//     // I'm so stupid, i thought akashic tome was on 1.20
-//     // Utils.server.runCommandSilent("give @p ")
-// })
+    // I'm so stupid, i thought akashic tome was on 1.20
+    // Utils.server.runCommandSilent("give @p ")
+});
+
+// PlayerEvents.respawned((event) => {
+//     event.server.runCommand(
+//         'give @p delightful:cheeseburger{RepairCost:0,display:{Name:\'{"text":"Starting Gift 2"}\'}}'
+//     );
+// });
