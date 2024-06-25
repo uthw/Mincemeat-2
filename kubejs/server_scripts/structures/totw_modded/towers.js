26caafc476a49af1045bf40978e1d1e41ff24de6
uthw <73855194+uthw@users.noreply.github.com>
2024-06-25 09:28:58 -0400

    more common overworld towers

---
+++ kubejs/server_scripts/structures/totw_modded/towers.js
@@ -0,0 +1,52 @@
+ServerEvents.highPriorityData((event) => {
+    // make towers more common in the overworld
+
+    let newData = {
+        structures: [
+            {
+                structure: "totw_modded:regular",
+                weight: 1,
+            },
+            {
+                structure: "totw_modded:desert",
+                weight: 1,
+            },
+            {
+                structure: "totw_modded:ice",
+                weight: 1,
+            },
+            {
+                structure: "totw_modded:derelict",
+                weight: 1,
+            },
+            {
+                structure: "totw_modded:derelict_grass",
+                weight: 1,
+            },
+            {
+                structure: "totw_modded:jungle",
+                weight: 1,
+            },
+            {
+                structure: "totw_modded:badlands",
+                weight: 1,
+            },
+            {
+                structure: "totw_modded:ocean_warm",
+                weight: 1,
+            },
+            {
+                structure: "totw_modded:ocean",
+                weight: 1,
+            },
+        ],
+        placement: {
+            type: "minecraft:random_spread",
+            spacing: 40, // 45
+            separation: 20, // 25
+            salt: 293423488,
+        },
+    };
+
+    event.addJson("totw_modded:worldgen/structure_set/overworld.json", newData);
+});
