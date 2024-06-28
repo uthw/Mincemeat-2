1e69f470d7421e357c79b55b5dd68852e82773fc
uthw <73855194+uthw@users.noreply.github.com>
2024-06-27 20:35:58 -0400

    Initial commit

---
+++ kubejs/startup_scripts/items/modificationArmor.js
@@ -0,0 +1,16 @@
+// ItemEvents.modification((event) => {
+//     let armors = {
+//         name: "",
+//         protection: 0,
+//         toughness: 0,
+//         knockback: 0,
+//     };
+
+//     armors.forEach((armor) => {
+//         event.modify(armor.name, (item) => {
+//             item.armorProtection = armor.protection;
+//             item.armorToughness = armor.toughness;
+//             item.armorKnockbackResistance = armor.knockback;
+//         });
+//     });
+// });
---
+++ kubejs/startup_scripts/items/modificationTool.js
@@ -0,0 +1,14 @@
+// ItemEvents.modification((event) => {
+//     let tools = {
+//         name: "",
+//         damage: 0,
+//         speed: 0,
+//     };
+
+//     tools.forEach((tool) => {
+//         event.modify(tool.name, (item) => {
+//             item.attackDamage = tool.damage;
+//             item.attackSpeed = tool.speed;
+//         });
+//     });
+// });
