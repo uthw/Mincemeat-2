1a2383f3cb3a123144453687bd7ec5d449d765c9
uthw <73855194+uthw@users.noreply.github.com>
2024-07-01 10:28:54 -0400

    Improve code

--- kubejs/startup_scripts/items/graveScroll.js
+++ kubejs/startup_scripts/items/graveScroll.js
@@ -32,154 +32,88 @@ global.gravescroll = (entity) => {
         "minecraft:water_breathing",
         "minecraft:slow_falling",
     ];
+
+    let targetDimension = "";
+
+    switch (pData.deathDimension) {
+        case "overworld":
+            targetDimension = "minecraft:overworld";
+            break;
+        case "the_nether":
+            targetDimension = "minecraft:the_nether";
+            break;
+        case "the_end":
+            targetDimension = "minecraft:the_end";
+            break;
+        case "black_bridge":
+            targetDimension = "minecells:black_bridge";
+            break;
+        case "dungeon":
+            targetDimension = "bloodmagic:dungeon";
+            break;
+        case "everbright":
+            targetDimension = "blue_skies:everbright";
+            break;
+        case "everdawn":
+            targetDimension = "blue_skies:everdawn";
+            break;
+        case "insufferable_crypt":
+            targetDimension = "minecells:insufferable_crypt";
+            break;
+        case "otherside":
+            targetDimension = "deeperdarker:otherside";
+            break;
+        case "past":
+            targetDimension = "graveyard:past";
+            break;
+        case "prison":
+            targetDimension = "minecells:prison";
+            break;
+        case "promenade":
+            targetDimension = "minecells:promenade";
+            break;
+        case "ramparts":
+            targetDimension = "minecells:ramparts";
+            break;
+        case "the_aether":
+            targetDimension = "aether:the_aether";
+            break;
+        case "undergarden":
+            targetDimension = "undergarden:undergarden";
+            break;
+        case "void":
+            targetDimension = "voidz:void";
+            break;
+        default:
+            targetDimension = "overworld";
+            break;
+    }
+
+    console.log(targetDimension);
+
     if (item.id == "kubejs:grave_scroll") {
-        let targetDimension = "";
-
-        switch (pData.deathDimension) {
-            case "overworld":
-                targetDimension = "minecraft:overworld";
-                break;
-            case "the_nether":
-                targetDimension = "minecraft:the_nether";
-                break;
-            case "the_end":
-                targetDimension = "minecraft:the_end";
-                break;
-            case "black_bridge":
-                targetDimension = "minecells:black_bridge";
-                break;
-            case "dungeon":
-                targetDimension = "bloodmagic:dungeon";
-                break;
-            case "everbright":
-                targetDimension = "blue_skies:everbright";
-                break;
-            case "everdawn":
-                targetDimension = "blue_skies:everdawn";
-                break;
-            case "insufferable_crypt":
-                targetDimension = "minecells:insufferable_crypt";
-                break;
-            case "otherside":
-                targetDimension = "deeperdarker:otherside";
-                break;
-            case "past":
-                targetDimension = "graveyard:past";
-                break;
-            case "prison":
-                targetDimension = "minecells:prison";
-                break;
-            case "promenade":
-                targetDimension = "minecells:promenade";
-                break;
-            case "ramparts":
-                targetDimension = "minecells:ramparts";
-                break;
-            case "the_aether":
-                targetDimension = "aether:the_aether";
-                break;
-            case "undergarden":
-                targetDimension = "undergarden:undergarden";
-                break;
-            case "void":
-                targetDimension = "voidz:void";
-                break;
-            default:
-                targetDimension = "overworld";
-                break;
-        }
-
-        console.log(targetDimension);
-
-        entity.server.runCommand(
-            `execute as ${entity.username} run effect give @p minecraft:resistance 8 4 true`
-        );
-
-        effects.forEach((effect) => {
-            console.log("Giving effect");
-            entity.server.runCommand(
-                `execute as ${entity.username} run effect give @p ${effect} 20 0 true`
-            );
-        });
-
-        entity.server.runCommand(
-            `execute in ${targetDimension} run tp ${entity.username} ${pData.deathx} ${pData.deathy} ${pData.deathz}`
-        );
-
+        applyEffectsAndTeleport(entity, targetDimension, effects);
         item.count--;
-    } else if (offHandItem.id == "kubejs:grave_scroll") {
-        let targetDimension = "";
+    }
 
-        switch (pData.deathDimension) {
-            case "overworld":
-                targetDimension = "minecraft:overworld";
-                break;
-            case "the_nether":
-                targetDimension = "minecraft:the_nether";
-                break;
-            case "the_end":
-                targetDimension = "minecraft:the_end";
-                break;
-            case "black_bridge":
-                targetDimension = "minecells:black_bridge";
-                break;
-            case "dungeon":
-                targetDimension = "bloodmagic:dungeon";
-                break;
-            case "everbright":
-                targetDimension = "blue_skies:everbright";
-                break;
-            case "everdawn":
-                targetDimension = "blue_skies:everdawn";
-                break;
-            case "insufferable_crypt":
-                targetDimension = "minecells:insufferable_crypt";
-                break;
-            case "otherside":
-                targetDimension = "deeperdarker:otherside";
-                break;
-            case "past":
-                targetDimension = "graveyard:past";
-                break;
-            case "prison":
-                targetDimension = "minecells:prison";
-                break;
-            case "promenade":
-                targetDimension = "minecells:promenade";
-                break;
-            case "ramparts":
-                targetDimension = "minecells:ramparts";
-                break;
-            case "the_aether":
-                targetDimension = "aether:the_aether";
-                break;
-            case "undergarden":
-                targetDimension = "undergarden:undergarden";
-                break;
-            case "void":
-                targetDimension = "voidz:void";
-                break;
-            default:
-                targetDimension = "overworld";
-                break;
-        }
-
-        console.log(targetDimension);
-
-        entity.server.runCommand(
-            `execute as ${entity.username} run effect give @p minecraft:resistance 10 4 true`
-        );
-
-        effects.forEach((effect) => {
-            entity.server.runCommand(
-                `execute as ${entity.username} run effect give @p ${effect} 20 0 true`
-            );
-        });
-
-        entity.server.runCommand(
-            `execute in ${targetDimension} run tp ${entity.username} ${pData.deathx} ${pData.deathy} ${pData.deathz}`
-        );
-
+    if (offHandItem.id == "kubejs:grave_scroll") {
+        applyEffectsAndTeleport(entity, targetDimension, effects);
         offHandItem.count--;
     }
 };
+
+function applyEffectsAndTeleport(entity, targetDimension, effects) {
+    entity.server.runCommand(
+        `execute as ${entity.username} at @s run effect give @s minecraft:resistance 8 4 true`
+    );
+
+    effects.forEach((effect) => {
+        entity.server.runCommand(
+            `execute as ${entity.username} at @s run effect give @s ${effect} 20 0 true`
+        );
+    });
+
+    entity.server.runCommand(
+        `execute as ${entity.username} in ${targetDimension} run tp @s ${entity.persistentData.deathx} ${entity.persistentData.deathy} ${entity.persistentData.deathz}`
+    );
+}
