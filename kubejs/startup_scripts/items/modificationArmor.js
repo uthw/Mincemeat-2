d22b86fe589c5e48e57190e752d169f13fe08ec1
uthw <73855194+uthw@users.noreply.github.com>
2024-07-04 20:26:21 -0400

    flint dmage

--- kubejs/startup_scripts/items/modificationArmor.js
+++ kubejs/startup_scripts/items/modificationArmor.js
@@ -1,16 +1,60 @@
-// ItemEvents.modification((event) => {
-//     let armors = {
-//         name: "",
-//         protection: 0,
-//         toughness: 0,
-//         knockback: 0,
-//     };
+ItemEvents.modification((event) => {
+    let armors = [
+        // {
+        //     name: "blue_skies:diopside_helmet",
+        //     protection: 2,
+        //     toughness: 5.5,
+        //     knockback: 0,
+        // },
+        // {
+        //     name: "blue_skies:diopside_chestplate",
+        //     protection: 7,
+        //     toughness: 5.5,
+        //     knockback: 0,
+        // },
+        // {
+        //     name: "blue_skies:diopside_leggings",
+        //     protection: 5,
+        //     toughness: 5.5,
+        //     knockback: 0,
+        // },
+        // {
+        //     name: "blue_skies:diopside_boots",
+        //     protection: 2,
+        //     toughness: 5.5,
+        //     knockback: 0,
+        // },
+        // {
+        //     name: "blue_skies:charoite_helmet",
+        //     protection: 4,
+        //     toughness: 2,
+        //     knockback: 0,
+        // },
+        // {
+        //     name: "blue_skies:charoite_chestplate",
+        //     protection: 9,
+        //     toughness: 2,
+        //     knockback: 0,
+        // },
+        // {
+        //     name: "blue_skies:charoite_leggings",
+        //     protection: 7,
+        //     toughness: 2,
+        //     knockback: 0,
+        // },
+        // {
+        //     name: "blue_skies:charoite_boots",
+        //     protection: 4,
+        //     toughness: 2,
+        //     knockback: 0,
+        // },
+    ];
 
-//     armors.forEach((armor) => {
-//         event.modify(armor.name, (item) => {
-//             item.armorProtection = armor.protection;
-//             item.armorToughness = armor.toughness;
-//             item.armorKnockbackResistance = armor.knockback;
-//         });
-//     });
-// });
+    armors.forEach((armor) => {
+        event.modify(armor.name, (item) => {
+            item.armorProtection = armor.protection;
+            item.armorToughness = armor.toughness;
+            item.armorKnockbackResistance = armor.knockback;
+        });
+    });
+});
--- kubejs/startup_scripts/items/modificationTool.js
+++ kubejs/startup_scripts/items/modificationTool.js
@@ -1,14 +1,66 @@
-// ItemEvents.modification((event) => {
-//     let tools = {
-//         name: "",
-//         damage: 0,
-//         speed: 0,
-//     };
+ItemEvents.modification((event) => {
+    let tools = [
+        {
+            name: "minecells:flint",
+            damage: 9,
+        },
+        // {
+        //     name: "blue_skies:bluebright_spear",
+        //     damage: 5.5,
+        // },
+        // {
+        //     name: "blue_skies:starlit_spear",
+        //     damage: 5.5,
+        // },
+        // {
+        //     name: "blue_skies:frostbright_spear",
+        //     damage: 5.5,
+        // },
+        // {
+        //     name: "blue_skies:lunar_spear",
+        //     damage: 5.5,
+        // },
+        // {
+        //     name: "blue_skies:dusk_spear",
+        //     damage: 5.5,
+        // },
+        // {
+        //     name: "blue_skies:maple_spear",
+        //     damage: 5.5,
+        // },
+        // {
+        //     name: "blue_skies:comet_spear",
+        //     damage: 5.5,
+        // },
+        // {
+        //     name: "blue_skies:soulbound_spear",
+        //     damage: 5.5,
+        // },
+        // Not buffing the charoite/diopside weapons for now since it would mean I need to buff netherite and tetra
+        // {
+        //     name: "blue_skies:diopside_sword",
+        //     damage: 9,
+        // },
+        // {
+        //     name: "blue_skies:diopside_axe",
+        //     damage: 12,
+        // },
+        // {
+        //     name: "blue_skies:charoite_sword",
+        //     damage: 7,
+        // },
+        // {
+        //     name: "blue_skies:charoite_axe",
+        //     damage: 9,
+        // },
+    ];
 
-//     tools.forEach((tool) => {
-//         event.modify(tool.name, (item) => {
-//             item.attackDamage = tool.damage;
-//             item.attackSpeed = tool.speed;
-//         });
-//     });
-// });
+    tools.forEach((tool) => {
+        event.modify(tool.name, (item) => {
+            let spd = tool.speed ? tool.speed : item.attackSpeed;
+
+            item.attackDamage = tool.damage;
+            // item.attackSpeed = spd;
+        });
+    });
+});
