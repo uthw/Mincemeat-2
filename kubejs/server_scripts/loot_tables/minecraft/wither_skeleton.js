27556c6ae3cc10b23c714789c659e99e6472b428
uthw <73855194+uthw@users.noreply.github.com>
2024-07-18 17:41:39 -0400

    Disable ArchPalette bones

--- kubejs/server_scripts/loot_tables/minecraft/wither_skeleton.js
+++ kubejs/server_scripts/loot_tables/minecraft/wither_skeleton.js
@@ -1,3 +1,21 @@
-EntityEvents.drops("minecraft:wither_skeleton", (event) => {
-    event.addDrop("iceandfire:witherbone", 0.5);
-});
+// EntityEvents.drops("minecraft:wither_skeleton", (event) => {
+//     event.addDrop("iceandfire:witherbone", 0.5);
+// });
+
+// Doesn't work for some reason
+// LootJS.modifiers(event => {
+//     event.disableGlobalModifier("architects_palette:wither_skeleton_bones")
+    
+// })
+
+// Replaces the wither skeleton bone drop from architects_palette with ice and fire wither bone
+ServerEvents.highPriorityData(event => {
+    let tweak = {
+        "type": "architects_palette:wither_skeleton_bones",
+        "conditions": [],
+        "bone": "iceandfire:witherbone",
+        "replaces": "minecraft:bone"
+      }
+
+    event.addJson("architects_palette:loot_modifiers/wither_skeleton_bones.json", tweak)
+})
