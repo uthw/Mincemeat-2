de09e0051043a37937b418adad4e5abfad634fe6
uthw <73855194+uthw@users.noreply.github.com>
2024-07-18 17:46:24 -0400

    ignore this

--- kubejs/startup_scripts/items/modificationTool.js
+++ kubejs/startup_scripts/items/modificationTool.js
@@ -63,4 +63,12 @@ ItemEvents.modification((event) => {
             // item.attackSpeed = spd;
         });
     });
+
+    let copperTools = ["pickaxe", "shovel", "axe", "hoe"];
+
+    copperTools.forEach((copperTool) => {
+        event.modify(`iceandfire:copper_${copperTool}`, (item) => {
+            item.digSpeed *= 1.5;
+        });
+    });
 });
