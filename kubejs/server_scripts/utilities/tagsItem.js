09e1210607407c28b272239a7f693a6c3419cdb4
uthw <73855194+uthw@users.noreply.github.com>
2024-07-27 15:12:53 -0400

    Tag changes for L2 (not currently enabled)

--- kubejs/server_scripts/utilities/tagsItem.js
+++ kubejs/server_scripts/utilities/tagsItem.js
@@ -6,9 +6,27 @@ ServerEvents.tags("item", (event) => {
         ["forge:death_worm_chitins", "iceandfire:deathworm_chitin_white"],
         ["forge:death_worm_chitins", "iceandfire:deathworm_chitin_red"],
         ["forge:bowls", "ecologics:coconut_husk"],
+        // ["curios:head", "l2artifacts:head"],
+        // ["curios:necklace", "l2artifacts:necklace"],
+        // ["curios:body", "l2artifacts:body"],
+        // ["curios:bracelet", "l2artifacts:bracelet"],
+        // ["curios:belt", "l2artifacts:belt"],
     ];
 
     add.forEach((addition) => {
         event.add(addition[0], addition[1]);
     });
+
+    // const removeAll = [
+    //     // Remove all items from the tag
+    //     "curios:artifact_head",
+    //     "curios:artifact_necklace",
+    //     "curios:artifact_body",
+    //     "curios:artifact_bracelet",
+    //     "curios:artifact_belt",
+    // ];
+
+    // removeAll.forEach((removal) => {
+    //     event.removeAll(removal);
+    // });
 });
