52d419142299d91fc558c0d4e2cb095b470c97bf
uthw <73855194+uthw@users.noreply.github.com>
2024-08-08 20:51:50 -0400

    Tetra items can now be enchanted at a table/infuser

---
+++ kubejs/server_scripts/utilities/tags/enchantmentCategories.js
@@ -0,0 +1,12 @@
+ServerEvents.tags("enchantment", (event) => {
+    let adds = [
+        ["minecraft:sword_or_axe", "tetra:modular_single"],
+        ["minecraft:sword_or_axe", "tetra:modular_sword"],
+        ["minecraft:sword_or_axe", "tetra:modular_polearm"],
+        ["minecraft:digger", "tetra:modular_double"],
+    ];
+
+    adds.forEach((add) => {
+        event.add(add[0], add[1]);
+    });
+});
