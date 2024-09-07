f25dc0e3726f31ca2f8e814dbaafae675f13e0bb
uthw <u.wood@icloud.com>
2024-09-07 15:28:59 -0400

    fix blue skies arcs duplicating if equipped on death fixes #120

--- kubejs/server_scripts/utilities/playerDeath.js
+++ kubejs/server_scripts/utilities/playerDeath.js
@@ -13,6 +13,20 @@ EntityEvents.death("minecraft:player", (event) => {
     pData.deathDimension = event.level.dimension.path;
     // global.mostRecentlyDied = player.username;
     console.log(global.mostRecentlyDied);
+
+    setTimeout(() => {
+        // This section is unrelated to the grave scroll item and made by me
+        // Hotfix for duplication glitch with Blue Skies arc (see #120)
+        // The blue skies devs have a fix for this on 1.20.4 and 1.21 but don't seem to plan on backporting it
+        event.entity.server.runCommand(
+            'kill @e[type=item,name="Ethereal Arc"]'
+        );
+        event.entity.server.runCommand('kill @e[type=item,name="Dusk Arc"]');
+        event.entity.server.runCommand('kill @e[type=item,name="Nature Arc"]');
+        event.entity.server.runCommand(
+            'kill @e[type=item,name="Poisonous Arc"]'
+        );
+    }, 100);
 });
 
 // TODO Wait for movement before giving a scroll
