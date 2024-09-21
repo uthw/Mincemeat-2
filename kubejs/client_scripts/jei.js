1841c4ea668f0048da13e57460615c4c92fc7f04
uthw <u.wood@icloud.com>
2024-09-21 16:22:09 -0400

    add information to some undocumented items fixes #151, #152, #153, and #154

---
+++ kubejs/client_scripts/jei.js
@@ -0,0 +1,36 @@
+JEIEvents.information((event) => {
+    let additions = [
+        {
+            item: "alexsmobs:ambergris",
+            desc: "Obtained by returning beached cachalot whales into the water without harming them.",
+        },
+        {
+            item: "jumbofurnace:jumbo_furnace",
+            desc: "Can be obtained as an item by shift-right clicking a Jumbo Furnace with shears.",
+        },
+        {
+            item: "eidolon:soul_shard",
+            desc: "Dropped by undead mobs when a Crystallization Ritual is activated nearby.",
+        },
+        {
+            item: "eidolon:unholy_symbol",
+            desc: "Obtained after completing enough chants. See quests for more information.",
+        },
+        {
+            item: "forbidden_arcanus:soulless_sand",
+            desc: "Soul Sand turns into this item when a Soul Extractor is used on it.",
+        },
+        {
+            item: "forbidden_arcanus:growing_edelwood",
+            desc: "Use a Corrupt Soul on a planted oak sapling.",
+        },
+        {
+            item: "forbidden_arcanus:soul",
+            desc: "Obtained when a Soul Extractor is used on Soul Sand.",
+        },
+    ];
+
+    additions.forEach((addition) => {
+        event.addItem(addition.item, [addition.desc]);
+    });
+});
