1a42dc88ff56f29e25d5c3f731de2473652e4cbe
uthw <u.wood@icloud.com>
2024-09-28 19:01:07 -0400

    let towers generate in more biomes

--- kubejs/server_scripts/utilities/tags/tagsBiome.js
+++ kubejs/server_scripts/utilities/tags/tagsBiome.js
@@ -1,6 +1,6 @@
 // This file mainly exists to add compatibility between structure mods and biome mods.
 // Cataclysm is a point of concern since its structures can only spawn in vanilla biomes.
-// Seems like BOMD structures can spawn in BetterNether biomes so I will leave those tags untouched for now.
+// biome tags can be looked up using /locate autofill, #forge is normally used by forge mods and #c is used by fabric mods
 
 ServerEvents.tags("worldgen/biome", (event) => {
     let betterNetherBiomes = [
@@ -31,5 +31,53 @@ ServerEvents.tags("worldgen/biome", (event) => {
     betterNetherBiomes.forEach((biome) => {
         event.add("cataclysm:has_structure/burning_arena_biomes", biome);
         event.add("cataclysm:has_structure/soul_black_smith_biomes", biome);
+    });
+});
+
+ServerEvents.highPriorityData((event) => {
+    // fix some towers not being able to spawn in modded biomes (usually things like modded deserts that don't have tags in vanilla)
+    let totwModdedTowers = [
+        {
+            key: "desert",
+            values: [
+                "minecraft:desert",
+                "wilderwild:oasis",
+                "#forge:desert",
+                "#forge:is_desert",
+                "#c:desert",
+            ],
+        },
+        {
+            key: "end",
+            values: ["#forge:is_outer_end"],
+        },
+        {
+            key: "ice",
+            values: [
+                "#biomeswevegone:snowy",
+                "#c:snowy",
+                "#c:is_snowy",
+                "#forge:snowy",
+                "#forge:is_snowy",
+                "#wilderwild:snowy_plains",
+            ],
+        },
+        {
+            key: "regular",
+            values: [
+                "#biomeswevegone:plains",
+                "#c:plains",
+                "#forge:is_plains",
+                "#forge:plains",
+                "#wilderwild:plains_grass",
+            ],
+        },
+    ];
+
+    totwModdedTowers.forEach((tower) => {
+        event.addJson(
+            `totw_modded:tags/worldgen/biome/has_structure/${tower.key}.json`,
+            tower
+        );
     });
 });
