9417d37b12d3ac35694f2e7f3163cd5710dc3c93
uthw <u.wood@icloud.com>
2025-01-08 22:40:11 -0500

    move this to the welcome quest

--- kubejs/server_scripts/utilities/book_clear.js
+++ kubejs/server_scripts/utilities/book_clear.js
@@ -3,25 +3,20 @@ PlayerEvents.advancement((event) => {
         return;
     }
 
-    // This advancement is granted right after a player loads into a world
-    // Utils.server.runCommand("clear @p patchouli:guide_book");
+    // // This advancement is granted right after a player loads into a world
+    // // Utils.server.runCommand("clear @p patchouli:guide_book");
 
-    // initial inventory
-    event.server.runCommand('clear @a patchouli:guide_book');
+    // // initial inventory
+    // event.server.runCommand('clear @a patchouli:guide_book');
 
-    event.server.runCommand("give @p ftbquests:book");
-    event.server.runCommand("clear @p enigmaticlegacy:cursed_ring");
-    event.server.runCommand(
-        'give @p delightful:smore{RepairCost:0,display:{Name:\'{"text":"Starting Gift"}\'}}'
-    );
+    // event.server.runCommand("give @p ftbquests:book");
+    // event.server.runCommand("clear @p enigmaticlegacy:cursed_ring");
+    // event.server.runCommand(
+    //     'give @p delightful:smore{RepairCost:0,display:{Name:\'{"text":"Starting Gift"}\'}}'
+    // );
 
-    event.server.runCommand("gamerule playersSleepingPercentage 50");
-    event.server.runCommand("gamerule persistentAnimals false");
+    // event.server.runCommand("gamerule playersSleepingPercentage 50");
 
-    // bow nerf. uuid only needs to not exist for any other arrow damage modifier
-    event.server.runCommand("attribute @p attributeslib:arrow_damage modifier add 03879795-c80d-42fb-953d-577b3dfb4aab nerf -0.2 multiply_base")
-
-    event.server.runCommand("gamerule artifacts.eternalSteak.enabled false");
-    event.server.runCommand("gamerule artifacts.everlastingBeef.enabled false");
-
+    // // bow nerf. uuid only needs to not exist for any other arrow damage modifier
+    // event.server.runCommand("attribute @p attributeslib:arrow_damage modifier add 03879795-c80d-42fb-953d-577b3dfb4aab nerf -0.2 multiply_base")
 });
