459cc89fa21cf236c9bf15d0ded95e606f73a458
uthw <u.wood@icloud.com>
2025-01-08 22:41:52 -0500

    add more gamerules here

--- kubejs/server_scripts/utilities/worldLoadedCommands.js
+++ kubejs/server_scripts/utilities/worldLoadedCommands.js
@@ -2,7 +2,12 @@
 // these are for things like balance changes so that way they can be applied in existing worlds after updating
 
 PlayerEvents.loggedIn((event) => {
-    let commands = ["gamerule artifacts.crystalHeart.healthBonus 6"];
+    let commands = [
+        "gamerule artifacts.crystalHeart.healthBonus 6",
+        "gamerule artifacts.eternalSteak.enabled false",
+        "gamerule artifacts.everlastingBeef.enabled false",
+        "gamerule persistentAnimals false",
+    ];
 
     commands.forEach((command) => {
         event.server.runCommandSilent(command);
