1f5482a7efbd638bee1e33402772e7e424e0a44a
uthw <u.wood@icloud.com>
2025-01-08 22:20:38 -0500

    disable eternal steak and make this easier to debug (#173)

--- kubejs/server_scripts/utilities/book_clear.js
+++ kubejs/server_scripts/utilities/book_clear.js
@@ -4,22 +4,24 @@ PlayerEvents.advancement((event) => {
     }
 
     // This advancement is granted right after a player loads into a world
-    // Utils.server.runCommandSilent("clear @p patchouli:guide_book");
+    // Utils.server.runCommand("clear @p patchouli:guide_book");
 
     // initial inventory
-    event.server.runCommandSilent("give @p ftbquests:book");
-    event.server.runCommandSilent("clear @p enigmaticlegacy:cursed_ring");
-    event.server.runCommandSilent(
+    event.server.runCommand('clear @a patchouli:guide_book');
+
+    event.server.runCommand("give @p ftbquests:book");
+    event.server.runCommand("clear @p enigmaticlegacy:cursed_ring");
+    event.server.runCommand(
         'give @p delightful:smore{RepairCost:0,display:{Name:\'{"text":"Starting Gift"}\'}}'
     );
 
-    event.server.runCommandSilent("gamerule playersSleepingPercentage 50");
-    event.server.runCommandSilent("gamerule persistentAnimals false");
+    event.server.runCommand("gamerule playersSleepingPercentage 50");
+    event.server.runCommand("gamerule persistentAnimals false");
 
     // bow nerf. uuid only needs to not exist for any other arrow damage modifier
     event.server.runCommand("attribute @p attributeslib:arrow_damage modifier add 03879795-c80d-42fb-953d-577b3dfb4aab nerf -0.2 multiply_base")
 
-    setTimeout(() => {
-        event.server.runCommandSilent("clear @p patchouli:guide_book");
-    }, 500);
+    event.server.runCommand("gamerule artifacts.eternalSteak.enabled false");
+    event.server.runCommand("gamerule artifacts.everlastingBeef.enabled false");
+
 });
