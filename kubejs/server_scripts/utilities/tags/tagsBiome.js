cdfab46ac5fe4f6ede6c4a6f044263fc4e0b1634
uthw <u.wood@icloud.com>
2025-01-13 22:29:05 -0500

    let some mobs spawn in more biomes

--- kubejs/server_scripts/utilities/tags/tagsBiome.js
+++ kubejs/server_scripts/utilities/tags/tagsBiome.js
@@ -2,37 +2,53 @@
 // Cataclysm is a point of concern since its structures can only spawn in vanilla biomes.
 // biome tags can be looked up using /locate autofill, #forge is normally used by forge mods and #c is used by fabric mods
 
-// ServerEvents.tags("worldgen/biome", (event) => {
-//     let betterNetherBiomes = [
-//         "betternether:bone_reef",
-//         "betternether:crimson_glowing_woods",
-//         "betternether:crimson_pinewood",
-//         "betternether:flooded_deltas",
-//         "betternether:gravel_desert",
-//         "betternether:magma_land",
-//         "betternether:nether_grasslands",
-//         "betternether:nether_jungle",
-//         "betternether:nether_mushroom_forest",
-//         "betternether:nether_mushroom_forest_edge",
-//         "betternether:nether_swampland",
-//         "betternether:nether_swampland_terraces",
-//         "betternether:old_fungiwoods",
-//         "betternether:old_swampwoods",
-//         "betternether:old_warped_woods",
-//         "betternether:poor_nether_grasslands",
-//         "betternether:soul_plain",
-//         "betternether:sulfuric_bone_reef",
-//         "betternether:upside_down_forest",
-//         "betternether:upside_down_forest_cleared",
-//         "betternether:wart_forest",
-//         "betternether:wart_forest_edge",
-//     ];
+ServerEvents.tags("worldgen/biome", (event) => {
+    let betterNetherBiomes = [
+        "betternether:bone_reef",
+        "betternether:crimson_glowing_woods",
+        "betternether:crimson_pinewood",
+        "betternether:flooded_deltas",
+        "betternether:gravel_desert",
+        "betternether:magma_land",
+        "betternether:nether_grasslands",
+        "betternether:nether_jungle",
+        "betternether:nether_mushroom_forest",
+        "betternether:nether_mushroom_forest_edge",
+        "betternether:nether_swampland",
+        "betternether:nether_swampland_terraces",
+        "betternether:old_fungiwoods",
+        "betternether:old_swampwoods",
+        "betternether:old_warped_woods",
+        "betternether:poor_nether_grasslands",
+        "betternether:soul_plain",
+        "betternether:sulfuric_bone_reef",
+        "betternether:upside_down_forest",
+        "betternether:upside_down_forest_cleared",
+        "betternether:wart_forest",
+        "betternether:wart_forest_edge",
+    ];
 
-//     betterNetherBiomes.forEach((biome) => {
-//         event.add("cataclysm:has_structure/burning_arena_biomes", biome);
-//         event.add("cataclysm:has_structure/soul_black_smith_biomes", biome);
-//     });
-// });
+    betterNetherBiomes.forEach((biome) => {
+        event.add("cataclysm:has_structure/burning_arena_biomes", biome);
+        event.add("cataclysm:has_structure/soul_black_smith_biomes", biome);
+    });
+
+    event.add("mowziesmobs:is_magical", "biomeswevegone:enchanted_tangle");
+    event.add("mowziesmobs:is_magical", "ars_nouveau:archwood_forest");
+    event.add("mowziesmobs:is_magical", "biomeswevegone:forgotten_forest");
+
+    event.add(
+        "buzzier_bees:has_animal/moobloom",
+        "biomeswevegone:allium_shrubland"
+    );
+    event.add(
+        "buzzier_bees:has_animal/moobloom",
+        "biomeswevegone:amaranth_grassland"
+    );
+    event.add("mowziesmobs:is_magical", "biomeswevegone:rose_fields");
+
+
+});
 
 // towers of the wild modded compat with worldgen mods
 ServerEvents.highPriorityData((event) => {
@@ -41,11 +57,11 @@ ServerEvents.highPriorityData((event) => {
             key: "desert",
             values: [
                 "minecraft:desert",
-                "wilderwild:oasis",
                 "#forge:desert",
                 "#forge:is_desert",
                 "#c:desert",
-                "#biomeswevegone:desert"
+                "#biomeswevegone:desert",
+                "#atmospheric:is_dunes",
             ],
         },
         {
@@ -60,8 +76,7 @@ ServerEvents.highPriorityData((event) => {
                 "#c:is_snowy",
                 "#forge:snowy",
                 "#forge:is_snowy",
-                "#wilderwild:snowy_plains",
-                "biomeswevegone:shattered_glacier"
+                "biomeswevegone:shattered_glacier",
             ],
         },
         {
@@ -71,7 +86,6 @@ ServerEvents.highPriorityData((event) => {
                 "#c:plains",
                 "#forge:is_plains",
                 "#forge:plains",
-                "#wilderwild:plains_grass",
                 "#biomeswevegone:swamp",
                 "#biomeswevegone:floral",
                 "#biomeswevegone:forest",
@@ -82,7 +96,7 @@ ServerEvents.highPriorityData((event) => {
                 "#forge:savanna/tree",
                 "#minecraft:is_savanna",
                 "#forge:swamp",
-                "#forge:is_swamp"
+                "#forge:is_swamp",
             ],
         },
         {
@@ -92,7 +106,6 @@ ServerEvents.highPriorityData((event) => {
                 "#c:plains",
                 "#forge:is_plains",
                 "#forge:plains",
-                "#wilderwild:plains_grass",
                 "#biomeswevegone:swamp",
                 "#biomeswevegone:floral",
                 "#biomeswevegone:forest",
@@ -103,8 +116,8 @@ ServerEvents.highPriorityData((event) => {
                 "#forge:savanna/tree",
                 "#minecraft:is_savanna",
                 "#forge:swamp",
-                "#forge:is_swamp"
-            ]
+                "#forge:is_swamp",
+            ],
         },
         {
             key: "ocean",
@@ -112,15 +125,15 @@ ServerEvents.highPriorityData((event) => {
                 "minecraft:deep_cold_ocean",
                 "minecraft:deep_frozen_ocean",
                 "biomeswevegone:dead_sea",
-            ]
+            ],
         },
         {
             key: "ocean_warm",
             values: [
                 "minecraft:deep_lukewarm_ocean",
                 "biomesevegone:lush_stacks",
-            ]
-        }
+            ],
+        },
     ];
 
     totwModdedTowers.forEach((tower) => {
