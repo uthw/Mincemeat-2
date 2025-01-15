730c9ad389680337b82c157806faa4ebe2af0d1c
uthw <u.wood@icloud.com>
2025-01-14 19:36:49 -0500

    fix worldgen crash with this

--- kubejs/server_scripts/worldgen/aquamirae/ice_maze.js
+++ kubejs/server_scripts/worldgen/aquamirae/ice_maze.js
@@ -1,5 +1,19 @@
 ServerEvents.highPriorityData((event) => {
     let tweak = {
+        structures: [
+            {
+                structure: "aquamirae:ice_maze/common",
+                weight: 5,
+            },
+            {
+                structure: "aquamirae:ice_maze/arch",
+                weight: 2,
+            },
+            {
+                structure: "aquamirae:ice_maze/spiral",
+                weight: 1,
+            },
+        ],
         placement: {
             type: "minecraft:random_spread",
             salt: 9191766080,
