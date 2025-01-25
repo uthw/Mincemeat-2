38c9133c38afacc338f9f8384fe2d9c24cfc7df6
uthw <u.wood@icloud.com>
2025-01-25 10:24:58 -0500

    cachalot whale goes after deeplings

---
+++ kubejs/server_scripts/utilities/tags/tagsEntity.js
@@ -0,0 +1,10 @@
+ServerEvents.tags("entity_type", event => {
+    // Cachalot whale has more prey
+    event.add("alexsmobs:cachalot_whale_targets", "cataclysm:deepling"); event.add("alexsmobs:cachalot_whale_targets", "cataclysm:deepling_brute");
+    event.add("alexsmobs:cachalot_whale_targets", "cataclysm:deepling_angler");
+    event.add("alexsmobs:cachalot_whale_targets", "cataclysm:deepling_priest");
+    event.add("alexsmobs:cachalot_whale_targets", "cataclysm:deepling_warlock");
+    event.add("alexsmobs:cachalot_whale_targets", "biomeswevegone:man_o_war");
+
+
+})
