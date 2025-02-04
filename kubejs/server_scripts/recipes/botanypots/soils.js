a4eadae565b9929a4516af61348d2429e2fa7ea0
uthw <u.wood@icloud.com>
2025-02-03 21:27:24 -0500

    speed up pots a little

--- kubejs/server_scripts/recipes/botanypots/soils.js
+++ kubejs/server_scripts/recipes/botanypots/soils.js
@@ -1,6 +1,6 @@
 // Nerf the growth speed of Botany Pots since I'm tired of complaints that they're too OP
 // The factor the original growth speed is multiplied by: 0.25 = 25% of the original speed or 4 times as slow
-let growthFactor = 0.25;
+let growthFactor = 0.4;
 
 ServerEvents.recipes((event) => {
     event.forEachRecipe({ type: "botanypots:soil" }, (recipe) => {
@@ -11,7 +11,7 @@ ServerEvents.recipes((event) => {
         let categories = recipeJson.categories;
         let growthModifier = recipeJson.growthModifier * growthFactor;
 
-        console.log(`Processing recipe: ${JSON.stringify(recipeJson)}`);
+        // console.log(`Processing recipe: ${JSON.stringify(recipeJson)}`);
 
         event
             .custom({
