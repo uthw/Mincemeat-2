39994979642554a437b0ac70ba5643cf95c071da
uthw <u.wood@icloud.com>
2025-02-03 21:12:57 -0500

    frostward ring warning fixes #193

--- kubejs/client_scripts/tooltip.js
+++ kubejs/client_scripts/tooltip.js
@@ -7,4 +7,5 @@ ItemEvents.tooltip((event) => {
     // event.addAdvanced()
 
     event.add("mekanism:flamethrower", "IMPORTANT: Crashes your game if you have shaders enabled");
+    event.add("irons_spellbooks:frostward_ring", "Do not equip this if you're on a server. A bug is causing this item to make unpleasant sounds for other players");
 });
