2fb7d3efd1f68be9416090c9f89973fa707c4d20
uthw <u.wood@icloud.com>
2025-02-13 01:05:06 -0500

    health refill now works in all dimensions fixes #126

--- kubejs/server_scripts/utilities/playerDeath.js
+++ kubejs/server_scripts/utilities/playerDeath.js
@@ -11,22 +11,32 @@ EntityEvents.death("minecraft:player", (event) => {
     pData.deathz = z.toFixed(0);
     pData.deathreset = 1;
     pData.deathDimension = event.level.dimension.path;
-    // global.mostRecentlyDied = player.username;
-    // console.log(global.mostRecentlyDied);
+    console.log("Dimension path: " + event.level.dimension.path);
+    console.log("Dimension namespace: " + event.level.dimension.namespace);
 
-    setTimeout(() => {
+    // health refill
+    event.server.runCommandSilent(
+        `execute in ${event.level.dimension.namespace}:${event.level.dimension.path} positioned ${x} ${y} ${z} as @e[type=!minecraft:player,distance=..50] run data modify entity @s Health set value 9999.0d`
+    );
+
+    event.server.scheduleInTicks(4, () => {
         // This section is unrelated to the grave scroll item and made by me
         // Hotfix for duplication glitch with Blue Skies arc (see #120)
         // The blue skies devs have a fix for this on 1.20.4 and 1.21 but don't seem to plan on backporting it
+
         event.entity.server.runCommand(
-            'kill @e[type=item,name="Ethereal Arc"]'
+            `execute in ${event.level.dimension.namespace}:${event.level.dimension.path} positioned ${pData.deathx} ${pData.deathy} ${pData.deathz} run kill @e[type=item,name="Dusk Arc"]`
         );
-        event.entity.server.runCommand('kill @e[type=item,name="Dusk Arc"]');
-        event.entity.server.runCommand('kill @e[type=item,name="Nature Arc"]');
         event.entity.server.runCommand(
-            'kill @e[type=item,name="Poisonous Arc"]'
+            `execute in ${event.level.dimension.namespace}:${event.level.dimension.path} positioned ${pData.deathx} ${pData.deathy} ${pData.deathz} run kill @e[type=item,name="Nature Arc"]`
         );
-    }, 100);
+        event.entity.server.runCommand(
+            `execute in ${event.level.dimension.namespace}:${event.level.dimension.path} positioned ${pData.deathx} ${pData.deathy} ${pData.deathz} run kill @e[type=item,name="Poisonous Arc"]`
+        );
+        event.entity.server.runCommand(
+            `execute in ${event.level.dimension.namespace}:${event.level.dimension.path} positioned ${pData.deathx} ${pData.deathy} ${pData.deathz} run kill @e[type=item,name="Ethereal Arc"]`
+        );
+    });
 });
 
 // TODO Wait for movement before giving a scroll
