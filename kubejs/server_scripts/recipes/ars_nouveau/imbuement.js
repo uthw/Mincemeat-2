578b1ad6dc8132c6b8db103ac28c4979548d2e1d
uthw <u.wood@icloud.com>
2025-02-15 17:40:10 -0500

    arcane crystal recipe (#142)

--- kubejs/server_scripts/recipes/ars_nouveau/imbuement.js
+++ kubejs/server_scripts/recipes/ars_nouveau/imbuement.js
@@ -3,10 +3,11 @@ ServerEvents.recipes((event) => {
         {
             input: "galosphere:allurite_shard",
             output: "forbidden_arcanus:arcane_crystal",
+            source: 500
         },
     ];
 
     recipes.forEach((recipe) => {
-        event.recipes.ars_nouveau.imbuement(recipe.input, recipe.output);
+        event.recipes.ars_nouveau.imbuement(recipe.input, recipe.output, recipe.source, []);
     });
 });
