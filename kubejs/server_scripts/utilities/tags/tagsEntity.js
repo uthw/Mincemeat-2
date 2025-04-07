8c644be547dbd4faf1573753607bc737d3a76258
uthw <u.wood@icloud.com>
2025-04-06 20:32:25 -0400

    add champions + enderman overhaul compat

--- kubejs/server_scripts/utilities/tags/tagsEntity.js
+++ kubejs/server_scripts/utilities/tags/tagsEntity.js
@@ -6,5 +6,26 @@ ServerEvents.tags("entity_type", event => {
     event.add("alexsmobs:cachalot_whale_targets", "cataclysm:deepling_warlock");
     event.add("alexsmobs:cachalot_whale_targets", "biomeswevegone:man_o_war");
 
+    event.add("champions:is_ender", "endermanoverhaul:badlands_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:cave_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:coral_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:crimson_forest_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:desert_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:end_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:end_islands_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:dark_oak_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:flower_fields_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:ice_spikes_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:mushroom_fields_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:nether_wastes_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:savanna_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:snowy_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:soulsand_valley_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:swamp_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:warped_forest_enderman");
+    event.add("champions:is_ender", "endermanoverhaul:windswept_hills_enderman");
+    event.add("champions:is_ender", "cataclysm:ender_golem");
+    event.add("champions:is_ender", "cataclysm:endermaptera");
+    event.add("champions:is_ender", "cataclysm:ender_guardian");
 
 })
