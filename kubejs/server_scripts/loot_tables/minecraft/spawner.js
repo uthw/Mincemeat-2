23652e47c7b0b3606b0b1ad8ea6e3ed5e6717093
uthw <u.wood@icloud.com>
2025-04-06 20:25:48 -0400

    rewrite with lootjs

--- kubejs/server_scripts/loot_tables/minecraft/spawner.js
+++ kubejs/server_scripts/loot_tables/minecraft/spawner.js
@@ -1,51 +1,7 @@
-ServerEvents.highPriorityData((event) => {
-    // let tweak = {
-    //     type: "minecraft:block",
-    //     pools: [
-    //         {
-    //             rolls: 1,
-    //             entries: [
-    //                 {
-    //                     type: "minecraft:item",
-    //                     name: "forbidden_arcanus:spawner_scrap",
-    //                 },
-    //             ],
-    //         },
-    //     ],
-    // };
 
-    // event.addJson("forbidden_arcanus:loot_tables/blocks/additions.json", tweak);
-
-    let tweak = {
-        type: "no_loot_type",
-        chance: 0.0001,
-        conditions: [
-            {
-                condition: "minecraft:inverted",
-                term: {
-                    condition: "minecraft:match_tool",
-                    predicate: {
-                        enchantments: [
-                            {
-                                enchantment: "minecraft:silk_touch",
-                                levels: {
-                                    min: 2,
-                                },
-                            },
-                        ],
-                    },
-                },
-            },
-            {
-                condition: "forge:loot_table_id",
-                loot_table_id: "minecraft:blocks/spawner",
-            },
-        ],
-        loot_addition: "forbidden_arcanus:blocks/additions/spawner_additions",
-    };
-
-    event.addJson(
-        "forbidden_arcanus:loot_modifiers/spawner_additions.json",
-        tweak
-    );
+LootJS.modifiers((event) => {
+    event
+        .addBlockLootModifier("minecraft:spawner")
+        .removeLoot("forbidden_arcanus:spawner_scrap")
+        .removeLoot("dungeonnowloading:spawner_fragment");
 });
