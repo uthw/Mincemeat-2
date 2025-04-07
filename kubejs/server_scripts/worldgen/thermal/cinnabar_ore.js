e3355f10b6e637a9938c86a3fa05252261149608
uthw <u.wood@icloud.com>
2025-04-06 20:34:15 -0400

    make cinnabar more common

--- kubejs/server_scripts/worldgen/thermal/cinnabar_ore.js
+++ kubejs/server_scripts/worldgen/thermal/cinnabar_ore.js
@@ -1,38 +1,34 @@
-// let replacement = {
-//     feature: "thermal:cinnabar_ore",
-//     placement: [
-//         {
-//             type: "thermal:config",
-//             config: "cinnabar_ore",
-//         },
-//         {
-//             type: "minecraft:count",
-//             count: 15,
-//         },
-//         {
-//             type: "minecraft:in_square",
-//         },
-//         {
-//             type: "minecraft:height_range",
-//             height: {
-//                 type: "minecraft:trapezoid",
-//                 max_inclusive: {
-//                     absolute: 48,
-//                 },
-//                 min_inclusive: {
-//                     absolute: -32,
-//                 },
-//             },
-//         },
-//         {
-//             type: "minecraft:biome",
-//         },
-//     ],
-// };
 
-// ServerEvents.highPriorityData((event) => {
-//     event.addJson(
-//         "thermal:worldgen/placed_feature/cinnabar_ore.json",
-//         replacement
-//     );
-// });
+ServerEvents.highPriorityData((event) => {
+    event.addJson("thermal:worldgen/placed_feature/cinnabar_ore.json", {
+        feature: "thermal:cinnabar_ore",
+        placement: [
+            {
+                type: "thermal:config",
+                config: "cinnabar_ore",
+            },
+            {
+                type: "minecraft:count",
+                count: 4,
+            },
+            {
+                type: "minecraft:in_square",
+            },
+            {
+                type: "minecraft:height_range",
+                height: {
+                    type: "minecraft:trapezoid",
+                    max_inclusive: {
+                        absolute: 32,
+                    },
+                    min_inclusive: {
+                        absolute: -48,
+                    },
+                },
+            },
+            {
+                type: "minecraft:biome",
+            },
+        ],
+    });
+});
