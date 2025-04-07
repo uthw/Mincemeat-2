bc2f24da363d191eb40ee5e9633b061c9a9ca3ef
uthw <u.wood@icloud.com>
2025-04-06 20:31:35 -0400

    remove xnet

--- kubejs/server_scripts/recipes/xnet/xnet.js
+++ kubejs/server_scripts/recipes/xnet/xnet.js
@@ -1,24 +1,24 @@
-ServerEvents.recipes((event) => {
-    let colors = ["red", "green", "blue", "yellow", "routing"];
-    // colors.forEach((item) => {
-    //     event.remove({ output: `xnet:advanced_connector_${item}` });
-    //     event.replaceOutput(
-    //         `xnet:connector_${item}`,
-    //         `xnet:advanced_connector_${item}`
-    //     );
+// ServerEvents.recipes((event) => {
+//     let colors = ["red", "green", "blue", "yellow", "routing"];
+//     // colors.forEach((item) => {
+//     //     event.remove({ output: `xnet:advanced_connector_${item}` });
+//     //     event.replaceOutput(
+//     //         `xnet:connector_${item}`,
+//     //         `xnet:advanced_connector_${item}`
+//     //     );
 
-    //     event.replaceInput(
-    //         `xnet:connector_${item}`,
-    //         `xnet:advanced_connector_${item}`
-    //     );
-    // });
+//     //     event.replaceInput(
+//     //         `xnet:connector_${item}`,
+//     //         `xnet:advanced_connector_${item}`
+//     //     );
+//     // });
 
-    event.shaped("xnet:controller", ["ABA", "CDC", "EFE"], {
-        A: "#thermal:rockwool",
-        B: "minecraft:comparator",
-        C: "#forge:dusts/redstone",
-        D: "thermal:machine_frame",
-        E: "#forge:ingots/electrum",
-        F: "create:precision_mechanism",
-    });
-});
+//     event.shaped("xnet:controller", ["ABA", "CDC", "EFE"], {
+//         A: "#thermal:rockwool",
+//         B: "minecraft:comparator",
+//         C: "#forge:dusts/redstone",
+//         D: "thermal:machine_frame",
+//         E: "#forge:ingots/electrum",
+//         F: "create:precision_mechanism",
+//     });
+// });
