b43afa979b834551ffb4151ea6f52493e0b712e6
uthw <u.wood@icloud.com>
2025-04-12 16:22:07 -0400

    fix #208

--- kubejs/client_scripts/tooltip.js
+++ kubejs/client_scripts/tooltip.js
@@ -8,4 +8,8 @@ ItemEvents.tooltip((event) => {
 
     event.add("mekanism:flamethrower", "IMPORTANT: Crashes your game if you have shaders enabled");
     event.add("irons_spellbooks:frostward_ring", "Do not equip this if you're on a server. A bug is causing this item to make unpleasant sounds for other players");
+    event.add("mekanismtools:refined_glowstone_boots", "IMPORTANT: Crashes your game if you apply an armor trim")
+    event.add("mekanismtools:refined_glowstone_leggings", "IMPORTANT: Crashes your game if you apply an armor trim")
+    event.add("mekanismtools:refined_glowstone_chestplate", "IMPORTANT: Crashes your game if you apply an armor trim")
+    event.add("mekanismtools:refined_glowstone_helmet", "IMPORTANT: Crashes your game if you apply an armor trim")
 });
