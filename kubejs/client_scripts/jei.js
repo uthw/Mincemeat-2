a24e27b856344ac1659a1d7aa07952e2defc9864
uthw <73855194+uthw@users.noreply.github.com>
2025-05-08 13:14:14 -0400

    add information on how to get some items most of these were either unclear before or changed in this version

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -30,23 +30,75 @@ JEIEvents.information((event) => {
         },
         {
             item: "create:scoria",
-            desc: "Created when flowing lava collides with a chocolate source block."
+            desc: "Created when flowing lava collides with a chocolate source block.",
         },
         {
             item: "create:limestone",
-            desc: "Created when flowing lava collides with a honey source block."
+            desc: "Created when flowing lava collides with a honey source block.",
         },
         {
             item: "neapolitan:mint_sprout",
-            desc: "Found in savannas and similar biomes."
+            desc: "Found in savannas and similar biomes.",
         },
         {
             item: "neapolitan:adzuki_beans",
-            desc: "Found in hilly biomes."
+            desc: "Found in hilly biomes.",
         },
         {
             item: "irons_spellbooks:permafrost_shard",
-            desc: "Found in impaled icebreaker shipwrecks."
+            desc: "Found in impaled icebreaker shipwrecks.",
+        },
+        {
+            item: "immersiveengineering:seed",
+            desc: "Found by breaking prairie grass (in the prairie biome).",
+        },
+        {
+            item: "occultism:datura_seeds",
+            desc: "Found by breaking beachgrass.",
+        },
+        {
+            item: "enigmaticlegacy:enigmatic_amulet",
+            desc: "Found by killing certain bosses. Red: Concierge, Light Blue: Conjurer, Purple: Wroughtnaut, Pink: Bellringer, Green: Berserker, Black: Ignis, Blue: Captain Cornelia",
+        },
+        {
+            item: "art_of_forging:vobrite_crystal",
+            desc: "Dropped by murmurs."
+        },
+        {
+            item: "art_of_forging:shards_of_malice",
+            desc: "Dropped by Ancient Knights - see supernatural magic quests for how to summon one."
+        },
+        {
+            item: "art_of_forging:fragment_of_eden",
+            desc: "Dropped in large quantities by the Corrupted Champion."
+        },
+        {
+            item: "art_of_forging:ancient_axe",
+            desc: "Dropped by Frostmaw."
+        },
+        {
+            item: "art_of_forging:ancient_blade",
+            desc: "Dropped by Ghost of Captain Cornelia."
+        },
+        {
+            item: "art_of_forging:ancient_flail",
+            desc: "Dropped by Night Lich."
+        },
+        {
+            item: "art_of_forging:soul_ember",
+            desc: "Obtained by killing a blaze on soul soil."
+        },
+        {
+            item: "art_of_forging:nano_insectoid",
+            desc: "Dropped from clawing forged storage containers in extractor ruins."
+        },
+        {
+            item: "art_of_forging:encoded_canister",
+            desc: "Dropped from clawing forged storage containers in extractor ruins."
+        },
+        {
+            item: "art_of_forging:esoteric_codex",
+            desc: "Dropped from clawing forged storage containers in extractor ruins."
         }
     ];
 
