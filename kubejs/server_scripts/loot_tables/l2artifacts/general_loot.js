eef491698f943511b0e9cb5ce713aacd5414a85f
uthw <73855194+uthw@users.noreply.github.com>
2025-05-08 19:48:47 -0400

    fix this script

--- kubejs/server_scripts/loot_tables/l2artifacts/general_loot.js
+++ kubejs/server_scripts/loot_tables/l2artifacts/general_loot.js
@@ -1,12 +1,9 @@
 LootJS.modifiers((event) => {
-    event
-        .addLootTableModifier(/^minecraft:chests\/.*/)
-        .addWeightedLoot(
-            [1, 1],
-            [
-                Item.of("l2artifacts:random_1").withChance(0.008),
-                Item.of("l2artifacts:random_2").withChance(0.007),
-                Item.of("l2artifacts:random_3").withChance(0.006),
-            ]
-        );
+    event.addLootTableModifier(/^.*chests\/.*/).addLoot(
+        LootEntry.of("l2artifacts:random_1").when((c) => c.randomChance(0.01)), // 1%
+
+        LootEntry.of("l2artifacts:random_2").when((c) => c.randomChance(0.008)), // 0.8%
+
+        LootEntry.of("l2artifacts:random_3").when((c) => c.randomChance(0.006)) // 0.6%
+    );
 });
