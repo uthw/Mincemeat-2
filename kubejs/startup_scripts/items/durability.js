3c1f2a74efcb80e418bdd5657948a112533b8b3d
uthw <73855194+uthw@users.noreply.github.com>
2025-05-29 12:48:11 -0400

    increase durability of coral spear and bardiche also clean up this script

--- kubejs/startup_scripts/items/durability.js
+++ kubejs/startup_scripts/items/durability.js
@@ -8,19 +8,49 @@ const fiftyPercent =
 const seventyFivePercent = "blacksmith_gavel|montus_strike|troll_weapon";
 const critTen = "katana|saber|dagger|rapier|magicbane|divider";
 
+// Items that this script will not touch
 let blacklist = [
-    "bosses_of_mass_destruction:earthdive_spear",
-    "galosphere:preserved_flesh",
-    "actuallyadditions:coffee_cup"
-]
+    "bosses_of_mass_destruction:earthdive_spear", // Crashes
+    "galosphere:preserved_flesh", // Food item
+    "actuallyadditions:coffee_cup", // Food item
+];
+
+let customDurabilities = [
+    {
+        id: "cataclysm:coral_spear",
+        durability: 600,
+    },
+    {
+        id: "cataclysm:coral_bardiche",
+        durability: 900,
+    },
+];
 
 ItemEvents.modification((event) => {
     Item.list.forEach((item) => {
-        // console.log(item.getDisplayName());
         if (item.maxDamage != 0) {
             // Double the durability of every item
             event.modify(item, (tool) => {
+                // Avoid modifying blacklisted items
                 if (blacklist.includes(tool.id.toString())) {
+                    return;
+                }
+
+                if (
+                    customDurabilities.some((d) => d.id === tool.id.toString())
+                ) {
+                    let customDurability = customDurabilities.find(
+                        (d) => d.id === tool.id.toString()
+                    );
+                    try {
+                        tool.maxDamage = customDurability.durability;
+                    } catch (error) {
+                        console.error(
+                            `Error setting custom durability for ${tool.id}:`,
+                            error
+                        );
+                    }
+
                     return;
                 }
 
@@ -28,7 +58,7 @@ ItemEvents.modification((event) => {
                     let armorAttribute = Item.of(item)
                         .getItem()
                         .getAttributes("generic.armor");
-                    
+
                     if (armorAttribute[0]) {
                         tool.maxDamage *= 4;
                         // console.log(`${tool.id} is armor`);
@@ -36,97 +66,10 @@ ItemEvents.modification((event) => {
                         tool.maxDamage *= 2;
                         // console.log(`${tool.id} is not armor`);
                     }
-
-                    // let isArmor = Item.of(item).getItem().getAttributes("generic.armor")[0]["amount"] != 0;
-
-                    // if (isArmor) {
-                    //     tool.maxDamage *= 4;
-                    //     console.log(`${tool.id} is armor`);
-                    // } else {
-                    //     tool.maxDamage *= 2;
-                    // }
-                } catch (error) { 
+                } catch (error) {
                     // console.log("Error when applying durability to " + tool.id.toString());
                 }
-                
             });
-
-            // // Hotfix lol
-            // if (!item.getId() == "bosses_of_mass_destruction:earthdive_spear") {
-            //     // Add armor penetration to heavy weapons and crit to stabbing weapons
-
-            //     let toolId = item.getId();
-
-            //     let matchFifteen = toolId.match(fifteenPercent);
-            //     let matchTwentyFive = toolId.match(twentyFivePercent);
-            //     let matchThirtyFive = toolId.match(thirtyFivePercent);
-            //     let matchFifty = toolId.match(fiftyPercent);
-            //     let matchSeventyFive = toolId.match(seventyFivePercent);
-            //     let matchCrit = toolId.match(critTen);
-
-            //     if (matchCrit) {
-            //         event.modify(item, (tool) => {
-            //             tool.addAttribute(
-            //                 "obscure_api:critical_hit",
-            //                 "ab3f55d3-645c-4f38-a497-9c13a33db5cf",
-            //                 "crit",
-            //                 0.1,
-            //                 "multiply_base"
-            //             );
-            //         });
-            //     }
-            //     if (matchSeventyFive) {
-            //         event.modify(item, (tool) => {
-            //             tool.addAttribute(
-            //                 "obscure_api:penetration",
-            //                 "42f502a6-5bd5-4c7b-9043-3cf5d484b049",
-            //                 "armor_pen",
-            //                 0.75,
-            //                 "multiply_base"
-            //             );
-            //         });
-            //     } else if (matchFifty) {
-            //         event.modify(item, (tool) => {
-            //             tool.addAttribute(
-            //                 "obscure_api:penetration",
-            //                 "42f502a6-5bd5-4c7b-9043-3cf5d484b049",
-            //                 "armor_pen",
-            //                 0.5,
-            //                 "multiply_base"
-            //             );
-            //         });
-            //     } else if (matchThirtyFive) {
-            //         event.modify(item, (tool) => {
-            //             tool.addAttribute(
-            //                 "obscure_api:penetration",
-            //                 "42f502a6-5bd5-4c7b-9043-3cf5d484b049",
-            //                 "armor_pen",
-            //                 0.35,
-            //                 "multiply_base"
-            //             );
-            //         });
-            //     } else if (matchTwentyFive) {
-            //         event.modify(item, (tool) => {
-            //             tool.addAttribute(
-            //                 "obscure_api:penetration",
-            //                 "42f502a6-5bd5-4c7b-9043-3cf5d484b049",
-            //                 "armor_pen",
-            //                 0.25,
-            //                 "multiply_base"
-            //             );
-            //         });
-            //     } else if (matchFifteen) {
-            //         event.modify(item, (tool) => {
-            //             tool.addAttribute(
-            //                 "obscure_api:penetration",
-            //                 "42f502a6-5bd5-4c7b-9043-3cf5d484b049",
-            //                 "armor_pen",
-            //                 0.15,
-            //                 "multiply_base"
-            //             );
-            //         });
-            //     }
-            // }
         }
     });
 });
