cd6d1921bf022a94fec50b3e400adf10fa9d527c
uthw <73855194+uthw@users.noreply.github.com>
2025-07-17 11:50:27 -0400

    commented out tooltips that don't work

--- kubejs/client_scripts/tooltip.js
+++ kubejs/client_scripts/tooltip.js
@@ -12,4 +12,10 @@ ItemEvents.tooltip((event) => {
     event.add("mekanismtools:refined_glowstone_leggings", "IMPORTANT: Crashes your game if you apply an armor trim")
     event.add("mekanismtools:refined_glowstone_chestplate", "IMPORTANT: Crashes your game if you apply an armor trim")
     event.add("mekanismtools:refined_glowstone_helmet", "IMPORTANT: Crashes your game if you apply an armor trim")
+
+    // #255. These do not work likely due to being overwritten by the mod's custom tooltips.
+    // event.add("traveloptics:forlorn_harbinger_boots", "Wearing a full set replaces other sources of night vision with its own toggleable one. Hold shift for more info.");
+    // event.add("traveloptics:forlorn_harbinger_leggings", "Wearing a full set replaces other sources of night vision with its own toggleable one. Hold shift for more info.");
+    // event.add("traveloptics:forlorn_harbinger_chestplate", "Wearing a full set replaces other sources of night vision with its own toggleable one. Hold shift for more info.");
+    // event.add("traveloptics:forlorn_harbinger_helmet", "Wearing a full set replaces other sources of night vision with its own toggleable one. Hold shift for more info.");
 });
