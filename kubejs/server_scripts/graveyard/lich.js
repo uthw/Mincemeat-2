387e4dae336e5bfdfa58459001bbf7662b9ce1cb
uthw <73855194+uthw@users.noreply.github.com>
2025-09-14 12:28:11 -0400

    fix inconsistent file location for this script

--- kubejs/server_scripts/graveyard/lich.js
+++
@@ -1,8 +0,0 @@
-LootJS.modifiers((event) => {
-    event
-        .addEntityLootModifier("graveyard:lich")
-        .addWeightedLoot(
-            [32, 64],
-            [Item.of("art_of_forging:fragment_of_eden")]
-        );
-});
