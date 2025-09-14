c2d5678ce0b30b75607b65c5bc1d497ef25ecd01
uthw <73855194+uthw@users.noreply.github.com>
2025-09-14 12:28:32 -0400

    add dark matter to jumbo furnace (#178)

--- kubejs/server_scripts/recipes/jumbofurnace/jumbo_smelting.js
+++ kubejs/server_scripts/recipes/jumbofurnace/jumbo_smelting.js
@@ -6,6 +6,12 @@ ServerEvents.recipes((event) => {
             result: { item: "forbidden_arcanus:obsidian_ingot" },
             experience: 0.1,
         },
+        {
+            type: "jumbofurnace:jumbo_smelting",
+            ingredients: [{ item: "forbidden_arcanus:edelwood_log" }],
+            result: { item: "forbidden_arcanus:dark_matter" },
+            experience: 0.4,
+        },
     ];
 
     recipes.forEach((recipe) => {
