463cc29d8f475c9fe95f8cc90a3b222357896025
uthw <73855194+uthw@users.noreply.github.com>
2025-11-26 14:14:36 -0500

    lower botany pot growth speed by 50% nobody will notice

--- kubejs/server_scripts/recipes/botanypots/soils.js
+++ kubejs/server_scripts/recipes/botanypots/soils.js
@@ -1,6 +1,6 @@
 // Nerf the growth speed of Botany Pots since I'm tired of complaints that they're too OP
 // The factor the original growth speed is multiplied by: 0.25 = 25% of the original speed or 4 times as slow
-let growthFactor = 0.4;
+let growthFactor = 0.2;
 
 ServerEvents.recipes((event) => {
     event.forEachRecipe({ type: "botanypots:soil" }, (recipe) => {
