2eed2001d6785e9e24c2e650e0e648df5b6d5a5c
uthw <u.wood@icloud.com>
2026-04-19 20:05:18 -0400

    how does this item keep ending getting into loot tables

--- kubejs/server_scripts/loot_tables/relics/general_loot.js
+++ kubejs/server_scripts/loot_tables/relics/general_loot.js
@@ -1,5 +1,6 @@
 LootJS.modifiers((event) => {
     event
         .addLootTableModifier(/^.*chests\/.*/)
-        .removeLoot("relics:arrow_quiver");
+        .removeLoot("relics:arrow_quiver")
+        .removeLoot("relics:infinity_ham");
 });
