84f816189ef7750be378c2375f410ee7085962e7
uthw <u.wood@icloud.com>
2026-04-23 21:15:34 -0400

    this might be an optimization

--- kubejs/server_scripts/utilities/itemGate.js
+++ kubejs/server_scripts/utilities/itemGate.js
@@ -7,17 +7,15 @@ const restrictions = [
     ["waystones:end_stone_waystone", "nether_access"],
 ];
 
-PlayerEvents.inventoryChanged((event) => {
-    if (event.player.isCreative()) return;
+restrictions.forEach((restriction) => { 
+    PlayerEvents.inventoryChanged(restriction[0], (event) => {
+        if (event.player.isCreative()) return;
 
-    restrictions.forEach((restriction) => {
-        if (event.item.id === restriction[0]) {
-            event.player.inventory.clear(restriction[0]);
-            event.player.tell(
-                Text.red(
-                    "The Waystone disintegrates in your hands, as you lack the knowledge to use it.",
-                ),
-            );
-        }
+        event.player.inventory.clear(restriction[0]);
+        event.player.tell(
+            Text.red(
+                "The Waystone disintegrates in your hands, as you lack the knowledge to use it.",
+            ),
+        );
     });
 });
