6c1fb1310641bf19cbff6f9b645d384516bdfda9
uthw <u.wood@icloud.com>
2026-04-25 13:19:14 -0400

    fix waystones always getting deleted

--- kubejs/server_scripts/utilities/itemGate.js
+++ kubejs/server_scripts/utilities/itemGate.js
@@ -11,6 +11,8 @@ restrictions.forEach((restriction) => {
     PlayerEvents.inventoryChanged(restriction[0], (event) => {
         if (event.player.isCreative()) return;
 
+        if (event.player.stages.has(restriction[1])) return;
+
         event.player.inventory.clear(restriction[0]);
         event.player.tell(
             Text.red(
