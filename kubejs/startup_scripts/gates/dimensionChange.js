ea4ae1ac414f2e1a281a6198d622bd69a2e26036
uthw <u.wood@icloud.com>
2026-04-25 14:47:01 -0400

    optimize dimension change event

--- kubejs/startup_scripts/gates/dimensionChange.js
+++ kubejs/startup_scripts/gates/dimensionChange.js
@@ -1,46 +1,37 @@
 // source: darklotus781 on discord
 
+const dimChanges = {
+    "minecraft:the_nether": "nether_access",
+    "minecraft:the_end": "end_access",
+    "blue_skies:everbright": "dimension_access",
+    "blue_skies:everdawn": "dimension_access",
+    "aether:the_aether": "nether_access",
+    "minecells:prison": "minecells_access",
+    "minecells:promenade": "minecells_access",
+    "minecells:ramparts": "minecells_access",
+    "minecells:black_bridge": "minecells_access",
+    "minecells:insufferable_crypt": "minecells_access",
+}
+
 ForgeEvents.onEvent(
     "net.minecraftforge.event.entity.EntityTravelToDimensionEvent",
     (event) => {
-        global.dimChangeEvent(event);
-    }
-);
+        let entity = event.getEntity();
+        if (!entity.isPlayer() || entity.isCreative()) return;
 
-global.dimChangeEvent = (event) => {
-    function addDimension(enteringDimension, neededStage) {
-        if (!event.getEntity().player) return;
-        let player = event.getEntity();
         let targetDimension = event.dimension.location();
-        let server = player.server;
+        let neededStage = dimChanges[targetDimension];
 
-        if (player.isCreative()) return;
-
-        if (targetDimension == enteringDimension) {
-            if (!player.stages.has(neededStage)) {
-                player.statusMessage = Text.of(
-                    "The portal doesn't seem to work..."
-                );
-                server.schedule(
-                    2 * 1000,
-                    () =>
-                        (player.statusMessage = Text.of(
-                            "The flow of the dimensions rejects you (check the quests)."
-                        ))
-                );
-                event.setCanceled(true);
-            }
+        if (neededStage && !entity.stages.has(neededStage)) {
+            entity.statusMessage = Text.of("The portal doesn't seem to work...");
+            entity.server.schedule(
+                2 * 1000,
+                () =>
+                    (entity.statusMessage = Text.of(
+                        "The flow of the dimensions rejects you (check the quests).",
+                    )),
+            );
+            event.setCanceled(true);
         }
     }
-
-    addDimension("minecraft:the_nether", "nether_access");
-    addDimension("minecraft:the_end", "end_access");
-    addDimension("blue_skies:everbright", "dimension_access");
-    addDimension("blue_skies:everdawn", "dimension_access");
-    addDimension("aether:the_aether", "nether_access");
-    addDimension("minecells:prison", "minecells_access");
-    addDimension("minecells:promenade", "minecells_access");
-    addDimension("minecells:ramparts", "minecells_access");
-    addDimension("minecells:black_bridge", "minecells_access");
-    addDimension("minecells:insufferable_crypt", "minecells_access");
-};
+);
