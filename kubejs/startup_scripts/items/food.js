7da0b27c1b58b5d6e7131e89b8070b37ad46257b
uthw <u.wood@icloud.com>
2026-06-08 17:35:59 -0400

    Fix many foods being bugged Fixes #331

--- kubejs/startup_scripts/items/food.js
+++ kubejs/startup_scripts/items/food.js
@@ -1,4 +1,6 @@
-// Decreases all saturation restoration from food, but increases all nutrition to compensate
+
+// This is to get the effect ID of a string
+const $ForgeRegistries = Java.loadClass('net.minecraftforge.registries.ForgeRegistries');
 
 const hungerRatio = 1.35;
 const saturationRatio = 0.42;
@@ -14,25 +16,50 @@ const foodOverride = {
 };
 
 ItemEvents.modification((event) => {
-    // Single global listener
     event.modify(/.*:/, (item) => {
-        // Only trigger if the item is edible
         if (item.foodProperties) {
             let id = item.id;
+            let fp = item.foodProperties;
 
-            // Store the base values before modification
-            let baseNutrition = item.foodProperties.nutrition;
-            let baseSaturation = item.foodProperties.saturationModifier;
+            // Get existing properties. This script only changes nutrition and saturation so we'll restore the others
+            let baseNutrition = fp.getNutrition();
+            let baseSaturation = fp.getSaturationModifier();
+            let isMeat = fp.isMeat();
+            let alwaysEdible = fp.canAlwaysEat();
+            let isFastFood = fp.isFastFood();
+            let effects = fp.getEffects(); 
 
             item.foodProperties = (food) => {
-                // Lookup override values, if they exist, otherwise calculate based on ratios
+                // Apply manual overrides or do so automatically
                 if (foodOverride[id] !== undefined) {
                     food.hunger(foodOverride[id]);
                 } else {
                     food.hunger(Math.floor(baseNutrition * hungerRatio));
                 }
-
                 food.saturation(baseSaturation * saturationRatio);
+
+                // Restore everything else
+                if (isMeat) food.meat();
+                if (alwaysEdible) food.alwaysEdible();
+                if (isFastFood) food.fastToEat();
+
+                if (effects && !effects.isEmpty()) {
+                    effects.forEach((pair) => {
+                        let effectInstance = pair.getFirst();
+                        let probability = pair.getSecond();
+
+                        // Get an instance of MobEffect
+                        let mobEffect = effectInstance.getEffect();
+                        
+                        // Get other properties of the effect instance that the food wants
+                        let effectId = $ForgeRegistries.MOB_EFFECTS.getKey(mobEffect).toString();
+                        let duration = effectInstance.getDuration();
+                        let amplifier = effectInstance.getAmplifier();
+
+                        // Give the food the effect as it had before
+                        food.effect(effectId, duration, amplifier, probability);
+                    });
+                }
             };
         }
     });
