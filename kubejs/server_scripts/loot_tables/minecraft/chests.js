f25dca4adbdeb4f7bd410bbe5552d22c2db445c8
uthw <u.wood@icloud.com>
2026-06-09 18:39:37 -0400

    Disable the umbrella

--- kubejs/server_scripts/loot_tables/minecraft/chests.js
+++ kubejs/server_scripts/loot_tables/minecraft/chests.js
@@ -2,5 +2,6 @@ LootJS.modifiers((event) => {
     event
         .addLootTableModifier(/.*\/chests\/.*/)
         .removeLoot("enigmaticlegacy:etherium_ore")
-        .removeLoot("enigmaticlegacy:eye_of_nebula");
+        .removeLoot("enigmaticlegacy:eye_of_nebula")
+        .removeLoot("artifacts:umbrella");
 });
