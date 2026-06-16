3d0cad144a242fc7285924214f037c2235acb5c4
uthw <u.wood@icloud.com>
2026-06-16 17:55:50 -0400

    Remove netherite scrap from ancient knight loot table

--- kubejs/server_scripts/loot_tables/irons_spellbooks/citadel_keeper.js
+++ kubejs/server_scripts/loot_tables/irons_spellbooks/citadel_keeper.js
@@ -1,8 +1,6 @@
 LootJS.modifiers((event) => {
     event
         .addEntityLootModifier("irons_spellbooks:citadel_keeper")
-        .addWeightedLoot(
-            [2, 5],
-            [Item.of("art_of_forging:shards_of_malice").withChance(1)]
-        );
+        .addWeightedLoot([2, 5], [Item.of("art_of_forging:shards_of_malice").withChance(1)])
+        .removeLoot("minecraft:netherite_scrap");
 });
