a8e8e9fc052f9ede600ff669b90000ff6d770c4d
uthw <u.wood@icloud.com>
2026-07-30 12:53:06 -0400

    Improve boss health refill mechanic

--- kubejs/server_scripts/utilities/playerDeath.js
+++ kubejs/server_scripts/utilities/playerDeath.js
@@ -1,8 +1,9 @@
-const $GameRules = Java.loadClass("net.minecraft.world.level.GameRules")
+const $GameRules = Java.loadClass("net.minecraft.world.level.GameRules");
+const r = 50;
 
 EntityEvents.death("minecraft:player", (event) => {
     // const { x, y, z } = event.player;
-    const { entity, player, server } = event;
+    const { entity, player, server, level } = event;
     if (!player) return;
     const { x, y, z } = player;
     let pData = player.persistentData;
@@ -13,7 +14,7 @@ EntityEvents.death("minecraft:player", (event) => {
     pData.deathDimension = event.level.dimension.path;
 
     // XP storage
-        let keepInvEnabled = event.server.gameRules.getBoolean($GameRules.RULE_KEEPINVENTORY);
+    let keepInvEnabled = event.server.gameRules.getBoolean($GameRules.RULE_KEEPINVENTORY);
 
     if (!keepInvEnabled) {
         let retainedXp = Math.floor(player.xp * 0.75);
@@ -24,12 +25,55 @@ EntityEvents.death("minecraft:player", (event) => {
         player.setXp(0);
     }
 
-    // health refill
+    // Refills a percentage of nearby bosses' health that diminishes with the number of players nearby.
     let dim = `${event.level.dimension.namespace}:${event.level.dimension.path}`;
-    event.server.runCommandSilent(
-        // This works in 0.5.1 but you can switch to runCommand if weird things are happening
-        `execute in ${dim} positioned ${x} ${y} ${z} unless entity @a[distance=2..10] as @e[distance=..50,type=#mincemeat:heal_on_player_death] run data modify entity @s Health set value 9999.0`,
-    );
+
+    let survivingPlayerCount = level
+        .getPlayers()
+        .filter((p) => p.distanceToEntity(player) <= r && p.uuid !== player.uuid && p.isAlive())
+        .size();
+
+    // bossesAndMinions is defined in tags/tagsEntity.js. I would use mincemeat:heal_on_player_death instead
+    // but idk how to read datapack tags with kubejs without commands.
+    let bosses = level
+        .getEntitiesWithin(
+            AABB.of(
+                player.x - r,
+                player.y - r,
+                player.z - r,
+                player.x + r,
+                player.y + r,
+                player.z + r,
+            ),
+        )
+        .filter((e) => bossesAndMinions.some((boss) => e.type.toString() === boss));
+
+    if (bosses.size() > 0) {
+        try {
+            for (let boss of bosses) {
+                // getHealth, getMaxHealth, and setHealth are not valid for some entities (like dropped items)
+                // I think this causes them to not be reported by probejs, however they work in game.
+                let currentHealth = boss.getHealth();
+                let maxHealth = boss.getMaxHealth();
+
+                let percentageToHeal = 1.0 / (survivingPlayerCount + 1); // 50% if 1 player, 33% if 2 players, so on
+                let healthToAdd = maxHealth * percentageToHeal;
+
+                // I don't think you can go over max health, but just in case
+                let newHealth = Math.min(currentHealth + healthToAdd, maxHealth);
+
+                boss.setHealth(newHealth);
+            }
+        } catch (error) {
+            console.log(
+                `Error while healing bosses on player death: ${error}. Falling back to old method.`,
+            );
+            event.server.runCommandSilent(
+                // This works in 0.5.1 but you can switch to runCommand if weird things are happening
+                `execute in ${dim} positioned ${x} ${y} ${z} unless entity @a[distance=2..10] as @e[distance=..50,type=#mincemeat:heal_on_player_death] run data modify entity @s Health set value 9999.0`,
+            );
+        }
+    }
 
     event.server.scheduleInTicks(4, () => {
         // This section is unrelated to the grave scroll item and made by me
--- kubejs/server_scripts/utilities/tags/tagsEntity.js
+++ kubejs/server_scripts/utilities/tags/tagsEntity.js
@@ -1,3 +1,102 @@
+const bossesAndMinions = [
+    "blue_skies:artificial_golem",
+    "iceandfire:fire_dragon",
+    "iceandfire:fire_dragon",
+    "iceandfire:ice_dragon",
+    "iceandfire:lightning_dragon",
+    "iceandfire:hydra",
+    "alexsmobs:void_worm",
+    "undergarden:forgotten_guardian",
+    "cataclysm:ignis",
+    "cataclysm:ender_golem",
+    "cataclysm:ender_guardian",
+    "cataclysm:netherite_monstrosity",
+
+    "blue_skies:summoner",
+    "blue_skies:alchemist",
+    "blue_skies:starlit_crusher",
+    "blue_skies:arachnarch",
+    "botania:pink_wither",
+    "botania:doppleganger",
+    "meetyourfight:bellringer",
+    "meetyourfight:dame_fortuna",
+    "meetyourfight:swampjaw",
+    "meetyourfight:bellringer",
+    "meetyourfight:rosalyne",
+    "meetyourfight:rose_spirit",
+    "mowziesmobs:ferrous_wroughtnaut",
+    "mowziesmobs:umvuthi",
+    "mowziesmobs:frostmaw",
+    "ars_nouveau:wilden_boss",
+    "aquamirae:maze_mother",
+    "aquamirae:captain_cornelia",
+
+    "lost_aether_content:aerwhale_king",
+    "aether:valkyrie_queen",
+    "aether:sun_spirit",
+    "aether:slider",
+    "deep_aether:eye_of_the_storm",
+
+    "minecells:concierge",
+    "minecells:conjunctivius",
+    "minecells:sewers_tentacle",
+
+    "mowziesmobs:umvuthana_crane",
+    "mowziesmobs:umvuthana_raptor",
+
+    "bosses_of_mass_destruction:lich",
+    "bosses_of_mass_destruction:obsidilith",
+    "bosses_of_mass_destruction:void_blossom",
+    "bosses_of_mass_destruction:gauntlet",
+
+    "alexscaves:forsaken",
+    "alexscaves:tremorzilla",
+    "alexscaves:tremorsaurus",
+    "alexscaves:hullbreaker",
+
+    "graveyard:lich",
+    "graveyard:ghoul",
+
+    "cataclysm:the_prowler",
+    "cataclysm:the_harbinger",
+    "cataclysm:ancient_remnant",
+    "cataclysm:the_leviathan",
+    "cataclysm:kobolediator",
+    "cataclysm:wadjet",
+    "cataclysm:maledictus",
+    "cataclysm:aptrgangr",
+
+    "irons_spellbooks:dead_king",
+    "irons_spellbooks:dead_king_corpse",
+    "irons_spellbooks:citadel_keeper",
+    "irons_spellbooks:fire_boss",
+
+    "conjurer_illager:conjurer",
+
+    "galosphere:berserker",
+    "galosphere:preserved",
+
+    "cataclysm:scylla",
+    "cataclysm:clawdian",
+
+    "traveloptics:the_nightwarden",
+
+    "dungeonnowloading:fairkeeper_ouros",
+    "dungeonnowloading:fairkeeper_boros",
+
+    "fdbosses:chesed",
+    "fdbosses:malkuth",
+    "fdbosses:geburah",
+
+    "block_factorys_bosses:infernal_dragon",
+    "block_factorys_bosses:kraken",
+    "block_factorys_bosses:sandworm",
+    "block_factorys_bosses:underworld_knight",
+    "block_factorys_bosses:yeti",
+
+    "netherman:azazel",
+];
+
 ServerEvents.tags("entity_type", (event) => {
     // Cachalot whale has more prey
     event.add("alexsmobs:cachalot_whale_targets", "cataclysm:deepling");
@@ -7,10 +106,7 @@ ServerEvents.tags("entity_type", (event) => {
     event.add("alexsmobs:cachalot_whale_targets", "cataclysm:deepling_warlock");
     event.add("alexsmobs:cachalot_whale_targets", "cataclysm:lionfish");
     // event.add("alexsmobs:cachalot_whale_targets", "biomeswevegone:man_o_war");
-    event.add(
-        "alexsmobs:cachalot_whale_targets",
-        "creeperoverhaul:ocean_creeper"
-    );
+    event.add("alexsmobs:cachalot_whale_targets", "creeperoverhaul:ocean_creeper");
 
     event.add("alexsmobs:orca_targets", "cataclysm:deepling");
     // event.add("alexsmobs:orca_targets", "cataclysm:deepling_brute");
@@ -30,125 +126,17 @@ ServerEvents.tags("entity_type", (event) => {
     event.add("champions:is_ender", "endermanoverhaul:dark_oak_enderman");
     event.add("champions:is_ender", "endermanoverhaul:flower_fields_enderman");
     event.add("champions:is_ender", "endermanoverhaul:ice_spikes_enderman");
-    event.add(
-        "champions:is_ender",
-        "endermanoverhaul:mushroom_fields_enderman"
-    );
+    event.add("champions:is_ender", "endermanoverhaul:mushroom_fields_enderman");
     event.add("champions:is_ender", "endermanoverhaul:nether_wastes_enderman");
     event.add("champions:is_ender", "endermanoverhaul:savanna_enderman");
     event.add("champions:is_ender", "endermanoverhaul:snowy_enderman");
-    event.add(
-        "champions:is_ender",
-        "endermanoverhaul:soulsand_valley_enderman"
-    );
+    event.add("champions:is_ender", "endermanoverhaul:soulsand_valley_enderman");
     event.add("champions:is_ender", "endermanoverhaul:swamp_enderman");
     event.add("champions:is_ender", "endermanoverhaul:warped_forest_enderman");
-    event.add(
-        "champions:is_ender",
-        "endermanoverhaul:windswept_hills_enderman"
-    );
+    event.add("champions:is_ender", "endermanoverhaul:windswept_hills_enderman");
     event.add("champions:is_ender", "cataclysm:ender_golem");
     event.add("champions:is_ender", "cataclysm:endermaptera");
     event.add("champions:is_ender", "cataclysm:ender_guardian");
-
-    let bossesAndMinions = [
-        "blue_skies:artificial_golem",
-        "iceandfire:fire_dragon",
-        "iceandfire:fire_dragon",
-        "iceandfire:ice_dragon",
-        "iceandfire:lightning_dragon",
-        "iceandfire:hydra",
-        "alexsmobs:void_worm",
-        "undergarden:forgotten_guardian",
-        "cataclysm:ignis",
-        "cataclysm:ender_golem",
-        "cataclysm:ender_guardian",
-        "cataclysm:netherite_monstrosity",
-
-        "blue_skies:summoner",
-        "blue_skies:alchemist",
-        "blue_skies:starlit_crusher",
-        "blue_skies:arachnarch",
-        "botania:pink_wither",
-        "botania:doppleganger",
-        "meetyourfight:bellringer",
-        "meetyourfight:dame_fortuna",
-        "meetyourfight:swampjaw",
-        "meetyourfight:bellringer",
-        "meetyourfight:rosalyne",
-        "meetyourfight:rose_spirit",
-        "mowziesmobs:ferrous_wroughtnaut",
-        "mowziesmobs:umvuthi",
-        "mowziesmobs:frostmaw",
-        "ars_nouveau:wilden_boss",
-        "aquamirae:maze_mother",
-        "aquamirae:captain_cornelia",
-
-        "lost_aether_content:aerwhale_king",
-        "aether:valkyrie_queen",
-        "aether:sun_spirit",
-        "aether:slider",
-        "deep_aether:eye_of_the_storm",
-
-        "minecells:concierge",
-        "minecells:conjunctivius",
-        "minecells:sewers_tentacle",
-
-        "mowziesmobs:umvuthana_crane",
-        "mowziesmobs:umvuthana_raptor",
-
-        "bosses_of_mass_destruction:lich",
-        "bosses_of_mass_destruction:obsidilith",
-        "bosses_of_mass_destruction:void_blossom",
-        "bosses_of_mass_destruction:gauntlet",
-
-        "alexscaves:forsaken",
-        "alexscaves:tremorzilla",
-        "alexscaves:tremorsaurus",
-        "alexscaves:hullbreaker",
-
-        "graveyard:lich",
-        "graveyard:ghoul",
-
-        "cataclysm:the_prowler",
-        "cataclysm:the_harbinger",
-        "cataclysm:ancient_remnant",
-        "cataclysm:the_leviathan",
-        "cataclysm:kobolediator",
-        "cataclysm:wadjet",
-        "cataclysm:maledictus",
-        "cataclysm:aptrgangr",
-
-        "irons_spellbooks:dead_king",
-        "irons_spellbooks:dead_king_corpse",
-        "irons_spellbooks:citadel_keeper",
-        "irons_spellbooks:fire_boss",
-
-        "conjurer_illager:conjurer",
-
-        "galosphere:berserker",
-        "galosphere:preserved",
-
-        "cataclysm:scylla",
-        "cataclysm:clawdian",
-
-        "traveloptics:the_nightwarden",
-
-        "dungeonnowloading:fairkeeper_ouros",
-        "dungeonnowloading:fairkeeper_boros",
-
-        "fdbosses:chesed",
-        "fdbosses:malkuth",
-        "fdbosses:geburah",
-
-        "block_factorys_bosses:infernal_dragon",
-        "block_factorys_bosses:kraken",
-        "block_factorys_bosses:sandworm",
-        "block_factorys_bosses:underworld_knight",
-        "block_factorys_bosses:yeti",
-
-        "netherman:azazel"
-    ];
 
     bossesAndMinions.forEach((boss) => {
         event.add("mincemeat:heal_on_player_death", boss);
