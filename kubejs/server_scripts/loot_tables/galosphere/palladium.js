0c6054fc4d2f83dde6c7defa23a13185ad1fffce
uthw <u.wood@icloud.com>
2026-08-17 17:38:22 -0400

    Fix silver nugget drops

--- kubejs/server_scripts/loot_tables/galosphere/palladium.js
+++ kubejs/server_scripts/loot_tables/galosphere/palladium.js
@@ -1,4 +1,9 @@
 LootJS.modifiers((event) => {
     // Remove galosphere:palladium_nugget from pillager loot tables
-    event.addEntityLootModifier("pillager").removeLoot("galosphere:palladium_nugget");
+    event.addEntityLootModifier("pillager").removeLoot("galosphere:silver_nugget");
+
+    event
+        .addEntityLootModifier("pillager")
+        .randomChance(0.35)
+        .addWeightedLoot([1, 4], [Item.of("caverns_and_chasms:silver_nugget")]);
 });
